9ea9610ab54d29ea6eb52f3f2b8f8fa384975475
Andrew R <arrooney@ualberta.ca>
2020-09-28 18:43:43 -0600

    fix: include the proper platform header file

--- ex2_services/Services/service_response.c
+++ ex2_services/Services/service_response.c
@@ -25,7 +25,12 @@
 
 #include "service_utilities.h"
 #include "services.h"
-#include "system.h"
+
+#if SYSTEM_APP_ID == _DEMO_APP_ID_
+#include "ex2_demo_software/system.h"
+#elif SYSTEM_APP_ID == _OBC_APP_ID_
+#include "ex2_obc_software/system.h"
+#endif
 
 extern Service_Queues_t service_queues;  // Implemented by the host platform
 
