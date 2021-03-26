28ca0a69436882be793fa3890ba1861dd806f26c
Robert <taylorr82@mymacewan.ca>
2021-03-25 21:07:12 -0600

    implement time and date

--- ex2_hal/gps/equipment_handler/source/skytraq_gps_driver.c
+++ ex2_hal/gps/equipment_handler/source/skytraq_gps_driver.c
@@ -7,7 +7,6 @@
 #include "os_task.h"
 #include "skytraq_gps_driver.h"
 
-
 bool GGA_ENABLED = false;
 bool GSA_ENABLED = false;
 bool GSV_ENABLED = false;
@@ -38,6 +37,7 @@ bool gps_skytraq_driver_init() {
     //TODO: maybe implement a way to request CRC and compare it with a stored CRC?
     return true;
 }
+
 ErrorCode gps_configure_message_types(bool GGA, bool GSA, bool GSV, bool RMC, uint8_t interval) {
     uint8_t GGA_int = GGA ? interval : 0;
     uint8_t GSA_int = GSA ? interval : 0;
@@ -54,36 +54,88 @@ ErrorCode gps_disable_NMEA_output() {
     return gps_configure_message_types(false,false,false,false, 0);
 }
 
-void extract_time(uint32_t _time, TickType_t correction, uint8_t *utc_hour, uint8_t *utc_minute, uint8_t *utc_second, uint16_t *utc_ms) {
+
+// returns true if date overflows
+bool extract_time(uint32_t _time, TickType_t correction, time_t *utc_time) {
     // _time is stored such that: hhmmssss
 
     int ms_since_logged = (xTaskGetTickCount()* portTICK_PERIOD_MS) - correction*portTICK_PERIOD_MS;
-    *utc_hour = _time/1000000;
-    *utc_minute = _time / 10000 % 100;
-    *utc_second = _time / 100 % 100;
-    *utc_ms = (_time % 100) * 10;
+    utc_time->hour = _time/1000000;
+    utc_time->minute = _time / 10000 % 100;
+    utc_time->second = _time / 100 % 100;
+    utc_time->ms = (_time % 100) * 10;
 
     // Apply time correction:
-    *utc_ms += ms_since_logged;
-    if (*utc_ms / 1000) {
-        *utc_second += *utc_ms/1000;
-        *utc_ms %= 1000;
+    utc_time->ms += ms_since_logged;
+    if (utc_time->ms / 1000) {
+        utc_time->second += utc_time->ms/1000;
+        utc_time->ms %= 1000;
     }
-    if (*utc_second / 60) {
-        *utc_minute += *utc_second/60;
-        *utc_second %= 60;
+    if (utc_time->second / 60) {
+        utc_time->minute += utc_time->second/60;
+        utc_time->second %= 60;
     }
-    if (*utc_minute / 60) {
-        *utc_hour += *utc_minute/60;
-        *utc_minute %= 60;
+    if (utc_time->minute / 60) {
+        utc_time->hour += utc_time->minute/60;
+        utc_time->minute %= 60;
     }
-    if (*utc_hour / 24) {
-        *utc_hour %= 24;
+    if (utc_time->hour / 24) {
+        utc_time->hour %= 24;
+        return true;
+    }
+    return false;
+}
+
+void extract_date(uint32_t date, bool date_overflow, date_t *utc_date) {
+    utc_date->day = date / 10000;
+    utc_date->month = date / 100 % 100;
+    utc_date->year = date % 100;
+
+    if (date_overflow) {
+        utc_date->day++;
+        switch(utc_date->month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            if (utc_date->day / 31) {
+                utc_date->day %= 31;
+                utc_date->month++;
+            } break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            if (utc_date->day / 30) {
+                utc_date->day %= 30;
+                utc_date->month++;
+            } break;
+        case 2:
+            // check if the year is a leap year
+            if (((utc_date->year % 4 == 0) && (utc_date->year % 100!= 0)) || (utc_date->year % 400 == 0)) {
+                if (utc_date->day / 29) {
+                    utc_date->day %= 29;
+                    utc_date->month++;
+                }
+            } else {
+                if (utc_date->day / 28) {
+                    utc_date->day %= 28;
+                    utc_date->month++;
+                }
+            } break;
+        }
+        if (utc_date->month / 12) {
+            utc_date->month %= 12;
+            utc_date->year++;
+        }
     }
     return;
 }
 
-bool gps_get_utc_time(uint8_t *utc_hour, uint8_t *utc_minute, uint8_t *utc_second, uint16_t *utc_ms) {
+bool gps_get_utc_time(time_t *utc_time) {
 
     // this will take GPRMC time if it is available, otherwise GPGGA
     bool GGA = GGA_ENABLED;
@@ -94,7 +146,7 @@ bool gps_get_utc_time(uint8_t *utc_hour, uint8_t *utc_minute, uint8_t *utc_secon
     if (RMC) {
         bool RMC_valid = NMEAParser_get_GPRMC(&RMC_s);
         if (RMC_valid) {
-            extract_time(RMC_s._time, RMC_s._logtime, utc_hour, utc_minute, utc_second, utc_ms);
+            extract_time(RMC_s._time, RMC_s._logtime, utc_time);
             return true;
         }
     }
@@ -102,13 +154,30 @@ bool gps_get_utc_time(uint8_t *utc_hour, uint8_t *utc_minute, uint8_t *utc_secon
     if (GGA) {
         bool GGA_valid = NMEAParser_get_GPGGA(&GGA_s);
         if (GGA_valid) {
-            extract_time(GGA_s._time, GGA_s._logtime, utc_hour, utc_minute, utc_second, utc_ms);
+            extract_time(GGA_s._time, GGA_s._logtime, utc_time);
             return true;
         }
     }
     return false;
 }
 
+bool gps_get_date(date_t *utc_date) {
+    // date is only present on gprmc
+    time_t utc_time;
+    bool RMC = RMC_ENABLED;
+    GPRMC_s RMC_s;
+
+    if (RMC) {
+        bool RMC_valid = NMEAParser_get_GPRMC(&RMC_s);
+        if (RMC_valid) {
+            // time will be used to correct for if we overflow to a new utc date
+            bool date_overflow = extract_time(RMC_s._time, RMC_s._logtime, &utc_time);
+            extract_date(RMC_s._date, date_overflow, utc_date);
+            return true;
+        }
+    }
+    return false;
+}
 
 
 
