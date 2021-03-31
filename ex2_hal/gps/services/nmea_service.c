456a92437d8f9c1bbdd7c9b8359c41a7d6950e6d
Robert <taylorr82@mymacewan.ca>
2021-03-31 17:37:31 -0600

    refactor a bunch of code

---
+++ ex2_hal/gps/services/nmea_service.c
@@ -0,0 +1,31 @@
+/*
+ * nmea_service.c
+ *
+ *  Created on: Mar. 25, 2021
+ *      Author: Robert Taylor
+ */
+#include "nmea_service.h"
+#include "FreeRTOS.h"
+#include "os_task.h"
+#include <string.h>
+#include "util/service_utilities.h"
+
+static char NMEA_buf[NMEASENTENCE_MAXLENGTH];
+
+void NMEA_service() {
+    ex2_log("NMEA task started");
+
+    init_NMEA();
+
+    for (;;) {
+        memset(NMEA_buf, 0, NMEASENTENCE_MAXLENGTH);
+
+        xQueueReceive(NMEA_queue, NMEA_buf, portMAX_DELAY);
+        int i;
+        for (i = 0; i < strlen(NMEA_buf); i++) {
+            NMEAParser_encode(NMEA_buf[i]);
+        }
+        ex2_log("NMEA message received: %s", NMEA_buf);
+
+    }
+}
---
+++ ex2_hal/gps/services/nmea_service.h
@@ -0,0 +1,18 @@
+/*
+ * nmea_service.h
+ *
+ *  Created on: Mar. 25, 2021
+ *      Author: Robert Taylor
+ */
+
+
+#ifndef EX2_HAL_EX2_GPS_SOFTWARE_SERVICES_INCLUDE_NMEA_SERVICE_H_
+#define EX2_HAL_EX2_GPS_SOFTWARE_SERVICES_INCLUDE_NMEA_SERVICE_H_
+
+#include "FreeRTOS.h"
+#include "os_task.h"
+#include "NMEAParser.h"
+
+void NMEA_service();
+
+#endif /* EX2_HAL_EX2_GPS_SOFTWARE_SERVICES_INCLUDE_NMEA_SERVICE_H_ */
