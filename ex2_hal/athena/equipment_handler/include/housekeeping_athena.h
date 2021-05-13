97fd03a6c826ae3d6fb830ea2db6e003349d65c0
Dustin Wagner <60157752+wagnerd97@users.noreply.github.com>
2021-05-13 13:01:39 -0600

    Create athena housekeeping files

---
+++ ex2_hal/athena/equipment_handler/include/housekeeping_athena.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2015  University of Alberta
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+/**
+ * @file    housekeeping_athena.h
+ * @author  Dustin Wagner
+ * @date    2021-05-13
+ */
+
+#ifndef HOUSEKEEPING_ATHENA_H
+#define HOUSEKEEPING_ATHENA_H
+
+/*Add includes for other athena files to get hk data from*/
+#include "tempsense_athena.h"
+
+/*Add housekeeping fields to this struct to be included in hk reports
+ATTENTION:
+  Basic data types and fixed arrays allowed
+    e.g.
+    int, uint16_t, char, float, char string[30]
+  Structs and dynamic types not allowed. will break generic read/write to file system
+    e.g.
+    char *string, struct {} dataSet;
+*/
+typedef struct {
+  long temparray[6];                     //Athena temperature array
+} athena_housekeeping;
+
+int Athena_getHK(athena_housekeeping* athena_hk);
+
+#endif
---
+++ ex2_hal/athena/equipment_handler/source/housekeeping_athena.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2015  University of Alberta
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+/**
+ * @file    housekeeping_athena.c
+ * @author  Dustin Wagner
+ * @date    2021-05-13
+ */
+
+#include "housekeeping_athena.h"
+
+/**
+ * @brief
+ * 		Athena composition housekeeping getter
+ * @details
+ * 		Contains calls to other functions that each return a portion of Athena
+ *    telemetry data. Generally reports back to housekeeping_service.
+ * @attention
+ * 		New components should follow the structure of the existing components.
+ *    All data needs to be deep copied
+ * @param athena_hk
+ * 		Pointer to struct of athena housekeeping data
+ * @return
+ * 		Last found error will be returned. else no error returned
+ */
+int Athena_getHK(athena_housekeeping* athena_hk) {
+  int temporary;
+  int return_code = 0;
+
+  /*Add athena housekeeping getters here and put fields in h file*/
+  temporary = gettemp_all(&athena_hk->temparray);
+  if (temporary != 0) return_code = temporary;
+
+  return return_code;
+}
+
