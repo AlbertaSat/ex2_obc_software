942771fb6cdb6ef31e81e7a6d941255583e70887
Robert <taylorr82@mymacewan.ca>
2021-05-19 12:01:38 -0600

    add general purpose service and reboot capabilities

---
+++ Services/include/general.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2021  University of Alberta
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/*
+ * general.h
+ *
+ *  Created on: May 14, 2021
+ *      Author: Robert Taylor
+ */
+
+#ifndef EX2_SERVICES_GENERAL
+#define EX2_SERVICES_GENERAL
+
+SAT_returnState start_general_service(void);
+
+typedef enum {
+  REBOOT = 0
+} General_Subtype;  // shared with EPS!
+
+typedef enum {
+    bootloader = 'B',
+    golden = 'G',
+    application = 'A'
+} reboot_mode;
+
+#endif /* EX2_SERVICES_GENERAL_ */
