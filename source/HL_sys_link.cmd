804c495c77968d6fd404615d13eaa86aee4173b8
Robert <taylorr82@mymacewan.ca>
2021-06-03 11:27:39 -0600

    move linker to user code section

--- source/HL_sys_link.cmd
+++ source/HL_sys_link.cmd
@@ -38,6 +38,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 /* USER CODE BEGIN (0) */
+#ifdef DO_NOT_COMPILE
 /* USER CODE END */
 /*----------------------------------------------------------------------------*/
 /* Linker Settings                                                            */
@@ -100,18 +101,70 @@ SECTIONS
 		   RUN_START(BlinkyStartAddr ), RUN_END(BlinkyEndAddr )
 
 	.ramIntvecs : {} load=FLASH0, run=RAMINTVECS, palign=8, LOAD_START(ramint_LoadStart), SIZE(ramint_LoadSize), RUN_START(ramint_RunStart)
-
 /* USER CODE END */
 }
 
 /* USER CODE BEGIN (5) */
+#endif
 /* USER CODE END */
 
 /*----------------------------------------------------------------------------*/
 /* Misc                                                                       */
 
 /* USER CODE BEGIN (6) */
-/* USER CODE END */
 /*----------------------------------------------------------------------------*/
+/* Linker Settings                                                            */
 
+--retain="*(.intvecs)"
+
+
+/*----------------------------------------------------------------------------*/
+/* Memory Map                                                                 */
+
+MEMORY
+{
+    VECTORS (X)  : origin=0x00018000 length=0x00000024
+    KERNEL  (RX) : origin=0x00018024 length=0x00008000
+    FLASH0  (RX) : origin=0x00020024 length=0x00200000 - 0x00020020 - 0x4
+    FLASH1  (RX) : origin=0x00200000 length=0x00200000
+    STACKS  (RW) : origin=0x08000000 length=0x00000800
+    KRAM    (RW) : origin=0x08000800 length=0x00000800
+    RAM     (RW) : origin=(0x08000800+0x00000800) length=(0x0007f800 - 0x00000800 - 0x8)
+    RAMINTVECS (RWX) : origin=0x0807FFF8 length=0x08
+
+	SDRAM  (RWX) : origin=0x80000000 length=0x07FFFFFF//experimental
+
+}
+
+/*----------------------------------------------------------------------------*/
+/* Section Configuration                                                      */
+
+SECTIONS
+{
+    .intvecs : {} > VECTORS
+    /* FreeRTOS Kernel in protected region of Flash */
+    .kernelTEXT  align(32) : {} > KERNEL
+    .cinit       align(32) : {} > KERNEL
+    .pinit       align(32) : {} > KERNEL
+    /* Rest of code to user mode flash region */
+    .text        align(32) : {} > FLASH0
+    .const       align(32) : {} > FLASH0
+    /* FreeRTOS Kernel data in protected region of RAM */
+    .kernelBSS    : {} > KRAM
+    .kernelHEAP   : {} > RAM
+    .bss          : {} > RAM
+    .data         : {} > RAM
+    .sysmem       : {} > RAM
+
+    FEE_TEXT_SECTION align(32) : {} > FLASH0
+    FEE_CONST_SECTION align(32): {} > FLASH0
+    FEE_DATA_SECTION : {} > RAM
+
+ 	.blinky_section :  RUN = SDRAM, LOAD = FLASH0
+		   LOAD_START(BlinkyLoadStart), LOAD_END(BlinkyLoadEnd),  LOAD_SIZE(BlinkySize),
+		   RUN_START(BlinkyStartAddr ), RUN_END(BlinkyEndAddr )
+
+	.ramIntvecs : {} load=FLASH0, run=RAMINTVECS, palign=8, LOAD_START(ramint_LoadStart), SIZE(ramint_LoadSize), RUN_START(ramint_RunStart)
+}
+/* USER CODE END */
 /*----------------------------------------------------------------------------*/
