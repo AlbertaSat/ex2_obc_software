d13c4d3a354e783a6d7da96680ff48da8591c0a0
Thomas <ganley@ualberta.ca>
2021-06-30 17:06:32 -0600

    Created functions for sending data to sband over spi

--- ex2_hal/sband/equipment_handler/include/spi.h
+++ ex2_hal/sband/equipment_handler/include/spi.h
@@ -6,7 +6,13 @@
 #define spi_H
 
 // Mocked spi functions
+
+#define SPI_SBAND_BUS_REG spiREG3
+
 void  spi_writeData(void);
 
+void SPIMasterTx(spiBASE_t * regset, uint16_t * data);
+
+void SPISbandTx(uint16_t * data);
 
 #endif /* spi_H */
---
+++ ex2_hal/sband/equipment_handler/source/spi.c
@@ -0,0 +1,40 @@
+
+/*
+ * Copyright (C) 2015  University of Alberta
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/**
+ * @file spi.c
+ * @author Thomas Ganley
+ * @date 2021-06-23
+ */
+
+#include "HL_spi.h"
+#include "spi.h"
+
+// Sends a single 16bit data value
+void SPIMasterTx(spiBASE_t * regset, uint16_t * data){
+
+    spiDAT1_t dataconfig1_t;
+
+    dataconfig1_t.CS_HOLD = FALSE;
+    dataconfig1_t.WDEL    = FALSE;//not sure
+    dataconfig1_t.DFSEL   = SPI_FMT_0;
+    dataconfig1_t.CSNR    = 0x00;//no CS lines
+
+    spiTransmitData(regset, &dataconfig1_t, 1, data);
+}
+
+void SPISbandTx(uint16_t * data){
+    SPIMasterTx(SPI_SBAND_BUS_REG, data);
+}
