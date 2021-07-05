0fd41297508116a5d1d0f97deefa14895d0adca2
Taylorr82 <60161614+Taylorr82@users.noreply.github.com>
2021-07-05 10:51:33 -0600

    Initial version of logger (#30)
    * Revert "Revert "Merge branch 'master' into master""
    
    This reverts commit 72b922c388ce8f8f55024d54a70dddb2ae5c2d59.
    This commit wasn't supposed to go to AlbertaSat yet. But it's important
    
    * Remove init_filesystem again
    
    * Fix bad merge
    
    * Add proper linker command from upstream
    
    * move general service start routine to ex2_services
    
    * remove missed include
    
    * Remove uneeded exclude
    
    * First version of logger service
    
    * Rename start functions and add to system tasks
    
    * resolve pull request comments

--- ex2_system/source/logger/logger.c
+++ ex2_system/source/logger/logger.c
@@ -13,145 +13,219 @@
  */
 /**
  * @file communication_service.c
- * @author Andrew R. Rooney
+ * @author Robert Taylor
  * @date 2021-03-06
  */
 #include "logger/logger.h"
 
-#include <stdio.h>
-#include <inttypes.h>
-#include <stdlib.h>
 #include <assert.h>
 #include <HL_hal_stdtypes.h>
 #include <FreeRTOS.h>
+#include "os_queue.h"
+#include "printf.h"
+#include <stdbool.h>
+#include <string.h>
+#include <redposix.h>
 
-struct circular_buf_t {
-    uint8_t * buffer;
-    size_t head;
-    size_t tail;
-    size_t max; //of the buffer
-    bool full;
-};
+#define DEFAULT_INPUT_QUEUE_LEN 10
+#define TASK_NAME_SIZE 13
+#define INPUT_QUEUE_ITEM_SIZE PRINT_BUF_LEN + TASK_NAME_SIZE
 
-cbuf_handle_t circular_buf_init(uint8_t* buffer, size_t size)
-{
-    configASSERT(buffer && size);
+static bool fs_init; // true if filesystem initialized
 
-    cbuf_handle_t cbuf = pvPortMalloc(sizeof(circular_buf_t));
-    configASSERT(cbuf);
+static xQueueHandle input_queue = NULL;
+static TaskHandle_t my_handle;
 
-    cbuf->buffer = buffer;
-    cbuf->max = size;
-    circular_buf_reset(cbuf);
+const char logger_file[] = "VOL0:/syslog.log";
+uint32_t logger_file_handle = 0;
 
-    configASSERT(circular_buf_empty(cbuf));
+static void test_logger_daemon(void *pvParameters);
 
-    return cbuf;
-}
+/**
+ * @brief
+ * Logs a string
+ * 
+ * @details
+ * Will either log a string to the filesystem or the UART
+ * Will log to filesystem if it can
+ * Will lot of UART if IS_FLATSAT is not defined
+ * Prepends calling task name and timestamp
+ */
+void ex2_log(const char *format, ...) {
+    const char main_name[] = "MAIN";
+    char *task_name;
 
-void circular_buf_reset(cbuf_handle_t cbuf)
-{
-    configASSERT(cbuf);
+    if (xTaskGetSchedulerState() == taskSCHEDULER_SUSPENDED) {
+        return;
+    }
 
-    cbuf->head = 0;
-    cbuf->tail = 0;
-    cbuf->full = FALSE;
-}
+    if (xTaskGetSchedulerState() != taskSCHEDULER_RUNNING) {
+        task_name = main_name;
+    } else {
+        task_name = pcTaskGetName(NULL);
+    }
 
-void circular_buf_free(cbuf_handle_t cbuf)
-{
-    configASSERT(cbuf);
-    free(cbuf);
-}
+    char buffer[PRINT_BUF_LEN + TASK_NAME_SIZE] = {0};
 
-bool circular_buf_full(cbuf_handle_t cbuf)
-{
-    configASSERT(cbuf);
+    va_list arg;
+    va_start(arg, format);
+    vsnprintf(buffer + TASK_NAME_SIZE, PRINT_BUF_LEN, format, arg);
+    va_end(arg);
+    snprintf(buffer, PRINT_BUF_LEN + TASK_NAME_SIZE, "[%.10s]%s", task_name, buffer + TASK_NAME_SIZE);
 
-    return cbuf->full;
-}
-
-bool circular_buf_empty(cbuf_handle_t cbuf)
-{
-    configASSERT(cbuf);
-
-    return (!cbuf->full && (cbuf->head == cbuf->tail));
-}
-
-size_t circular_buf_capacity(cbuf_handle_t cbuf)
-{
-    configASSERT(cbuf);
-
-    return cbuf->max;
-}
-
-size_t circular_buf_size(cbuf_handle_t cbuf)
-{
-    configASSERT(cbuf);
-
-    size_t size = cbuf->max;
-
-    if(!cbuf->full)
-    {
-        if(cbuf->head >= cbuf->tail)
-        {
-            size = (cbuf->head - cbuf->tail);
-        }
-        else
-        {
-            size = (cbuf->max + cbuf->head - cbuf->tail);
+    int string_len = strlen(buffer);
+    if (buffer[string_len - 1] == '\n') {
+        buffer[string_len - 1] = '\0';
+        if (buffer[string_len - 2] == '\r') {
+            buffer[string_len -2] = '\0';
         }
     }
 
-    return size;
-}
-
-static void advance_pointer(cbuf_handle_t cbuf)
-{
-    configASSERT(cbuf);
-
-    if(cbuf->full)
-    {
-        cbuf->tail = (cbuf->tail + 1) % cbuf->max;
+    if (input_queue == NULL) {
+        do_output(buffer);
+        return;
+    } else {
+        xQueueSend(input_queue, buffer, 0); // will not wait for queue to have space
     }
 
-    cbuf->head = (cbuf->head + 1) % cbuf->max;
-    cbuf->full = (cbuf->head == cbuf->tail);
+    return;
 }
 
-static void retreat_pointer(cbuf_handle_t cbuf)
-{
-    configASSERT(cbuf);
+/**
+ * @brief
+ * Puts a string on the output
+ * 
+ * @details
+ * If filesystem is initialized it will write to the syslog.
+ * If the MCU it is being run on is a flatsat it will not print on the uart
+ * Prepends the uptime in seconds
+ * 
+ * @param String to print
+ * @return None
+ */
+static void do_output(const char *str) {
+    char output_string[STRING_MAX_LEN] = {0};
 
-    cbuf->full = FALSE;
-    cbuf->tail = (cbuf->tail + 1) % cbuf->max;
-}
+    uint32_t uptime = (uint32_t)(xTaskGetTickCount()/configTICK_RATE_HZ);
 
-void circular_buf_put(cbuf_handle_t cbuf, uint8_t data)
-{
-    configASSERT(cbuf && cbuf->buffer);
+    snprintf(output_string, STRING_MAX_LEN, "[%010d]%s\r\n", uptime, str);
 
-    cbuf->buffer[cbuf->head] = data;
-
-    advance_pointer(cbuf);
-}
-
-int circular_buf_get(cbuf_handle_t cbuf, uint8_t * data)
-{
-    configASSERT(cbuf && data && cbuf->buffer);
-
-    int r = -1;
-
-    if(!circular_buf_empty(cbuf))
-    {
-        *data = cbuf->buffer[cbuf->tail];
-        retreat_pointer(cbuf);
-
-        r = 0;
+    if (fs_init) {
+        red_write(logger_file_handle, output_string, strlen(output_string));
     }
 
-    return r;
+#ifndef IS_FLATSAT
+    printf(output_string);
+#endif
 }
 
+/**
+ * @brief
+ * Initialize filesystem related contructs for logger
+ * 
+ * @details
+ * Opens syslog file and sets fs_init to true on success
+ */
+bool init_logger_fs() {
+    if (fs_init) {
+        return true;
+    }
 
+    int32_t fd = red_open(logger_file, RED_O_RDWR | RED_O_APPEND);
+    if (fd < 0) {
+        int errno = red_errno;
+        fd = red_open(logger_file, RED_O_CREAT | RED_O_RDWR);
+        if (fd < 0) {
+            errno = red_errno;
+            return false;
+        }
+    }
+    fs_init = true;
+    logger_file_handle = fd;
+    return true;
+}
 
+/**
+ * @brief
+ * Shut down filesystem related constructs for logger
+ * 
+ * @details
+ * Closes syslog file and sets fs_init to false
+ */
+void stop_logger_fs() {
+    if (!fs_init) {
+        return;
+    }
+    red_close(logger_file_handle);
+    logger_file_handle = 0;
+    fs_init = false;
+}
+
+/**
+ * @brief
+ * Logger Daemon. Handle string data sent to the log system. Send with ex2_log
+ *
+ * @param pvParameters
+ *    task parameters (not used)
+ */
+static void logger_daemon(void *pvParameters) {
+    char buffer[PRINT_BUF_LEN] = {0};
+
+    if (!init_logger_fs()) {
+        // On the flatsat this will be unreported
+        ex2_log("Failed to initialize logger file");
+    }
+
+    input_queue = xQueueCreate(DEFAULT_INPUT_QUEUE_LEN, INPUT_QUEUE_ITEM_SIZE);
+
+    for ( ;; ) {
+        xQueueReceive(input_queue, buffer, portMAX_DELAY);
+        if (!fs_init) {
+            init_logger_fs(); // just keep trying
+        }
+        do_output(buffer);
+        memset(buffer, 0, PRINT_BUF_LEN);
+    }
+}
+
+// Logs a couple strings every second
+void test_logger_daemon(void *pvParameters) {
+    vTaskDelay(10000);
+    for (;;) {
+        ex2_log("TEST TASK!");
+        ex2_log("Test %d TASK", 5);
+        ex2_log("Test Task\r\n");
+        vTaskDelay(1000);
+    }
+}
+
+/**
+ * @brief
+ * Start the logger daemon
+ * 
+ * @param handle
+ *    Pointer as a return for the task handle
+ *
+ * @returns status
+ *   error report of task creation
+ */
+SAT_returnState start_logger_daemon(TaskHandle_t *handle) {
+    if (xTaskCreate((TaskFunction_t)logger_daemon,
+                  "logger", 1000, 0, LOGGER_TASK_PRIO,
+                  handle) != pdPASS) {
+        ex2_log("FAILED TO CREATE TASK logger\n");
+        return SATR_ERROR;
+    }
+    my_handle = *handle;
+    ex2_log("Logger Service Started");
+    return SATR_OK;
+}
+
+/**
+ * Kill the logger daemon gracefully
+ */
+void kill_logger_daemon() {
+    vTaskDelete(my_handle);
+    stop_logger_fs();
+    vQueueDelete(input_queue);
+}
