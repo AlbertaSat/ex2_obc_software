3b2d441701c00294c55025e0ccb5dcfd65d0ca06
Andrew R. Rooney <andrew@missioncontrolspaceservices.com>
2021-08-04 23:02:03 -0400

    add pre-commit hook

---
+++ x.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("asdasdfssadfasdasdfaasdfiasdfadsffffdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+           "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
+    return 0;
+}
