b94f27b4286737d76b0f97a1127b43d635d2c50f
Andrew R. Rooney <andrew@missioncontrolspaceservices.com>
2021-08-09 20:48:18 -0400

    change hooksPath in install script

--- install-hooks.sh
+++ install-hooks.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo ln -sf $SCRIPT_DIR/.git-hooks/hooks $SCRIPT_DIR/.git/hooks
+git config core.hooksPath .git-hooks/hooks/
