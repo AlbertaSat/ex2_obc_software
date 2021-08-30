d749ead07958120891e1a5c3faba7d0693b59fa6
Dustin Wagner <60157752+wagnerd97@users.noreply.github.com>
2021-08-30 12:28:19 -0600

    try to fix tests

--- test/test_ex2_obc_software/ex2_system/source/logger/test_logger.c
+++ test/test_ex2_obc_software/ex2_system/source/logger/test_logger.c
@@ -133,14 +133,14 @@ Ensure(logger, notices_file_does_not_exist) {
 }
 
 Ensure(logger, notices_file_exists) {
+    uint64_t size = 1000;
     REDSTATUS errnum = RED_ENOENT;
     expect(red_errnoptr, will_return(&errnum));
     expect(red_close, will_return(0));
     expect(red_unlink, will_return(0));
 
     expect(red_open, will_return(1));
-    expect(red_fstat, will_return(0));
-    REDSTAT file_stats.st_size = 1000;
+    expect(red_fstat, will_return(0), will_set_contents_of_parameter(pStat.st_size, &size, sizeof(uint64_t)));
     expect(red_close);
     expect(red_rename);
     expect(red_open, will_return(1));
@@ -168,14 +168,14 @@ Ensure(logger, sets_internals_when_file_does_not_exist) {
 }
 
 Ensure(logger, sets_internals_when_file_exists) {
+    uint64_t size = 1000;
     REDSTATUS errnum = RED_ENOENT;
     expect(red_errnoptr, will_return(&errnum));
     expect(red_close, will_return(0));
     expect(red_unlink, will_return(0));
 
     expect(red_open, will_return(1));
-    expect(red_fstat, will_return(0));
-    REDSTAT file_stats.st_size = 1000;
+    expect(red_fstat, will_return(0), will_set_contents_of_parameter(pStat.st_size, &size, sizeof(uint64_t)));
     expect(red_close);
     expect(red_rename);
     expect(red_open, will_return(1));
