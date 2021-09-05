31c2ba303e799b5b8e08d8e153b75014014719eb
Robert <taylorr82@mymacewan.ca>
2021-09-05 16:19:38 -0600

    add new commands

--- configure_repo.sh
+++ configure_repo.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+echo "Initializing submodules"
+git submodule init 
+exho "Updating submodules"
+git submodule update
 echo "Switching submodules to master"
 git submodule foreach 'git switch master || :'
 echo "Making sure submodules are up to date"
