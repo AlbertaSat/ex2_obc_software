432ab1d6d8a9d90fa293ecb65ef377aa84060a97
Robert <taylorr82@mymacewan.ca>
2021-09-05 15:29:48 -0600

    fix include

--- ex2_hal/sband/equipment_handler/include/spi.h
+++ ex2_hal/sband/equipment_handler/include/spi.h
@@ -10,6 +10,8 @@
 #define SPI_SBAND_BUS_REG spiREG3
 #define SPI_SBAND_DEF_FMT 1 // Defines SPI speed (~8.3 MHz)
 
+#include "HL_spi.h"
+
 void spi_writeData(void);
 
 void SPIMasterTx(spiBASE_t *regset, uint16_t *data, uint8_t format);
