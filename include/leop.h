39794637c9396de8193185823badfa06d164ee31
Grace Yi <yunqing@ualberta.ca>
2021-11-15 23:46:12 -0700

    clean up leop header

--- include/leop.h
+++ include/leop.h
@@ -9,25 +9,16 @@
 #define LEOP_H_
 
 #include <FreeRTOS.h>
-#include <csp/csp.h>
-#include <csp/drivers/usart.h>
-#include <csp/interfaces/csp_if_can.h>
 #include <os_task.h>
-#include <performance_monitor/system_stats.h>
-#include <redconf.h>
-#include <redfs.h>
-#include <redfse.h>
-#include <redposix.h>
-#include <redvolume.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <util/service_utilities.h>
 
-#include "../include/leop.h"
+#include "leop.h"
 #include "../ex2_hal/ex2_athena_software/equipment_handler/include/deployablescontrol.h"
-#include "../include/leop_eeprom.h"
+//#include "deployablescontrol.h"
+#include "leop_eeprom.h"
 
 void leop_init();
 bool hard_switch_status();
