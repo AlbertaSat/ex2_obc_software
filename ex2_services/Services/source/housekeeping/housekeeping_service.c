653a387811f6caa3d2f78b6a9af8a85e5f01b859
Nick Sorensen <nsorense@ualberta.ca>
2021-11-19 12:29:40 -0700

    Changing housekeeping code for the Hyperion panels.

--- ex2_services/Services/source/housekeeping/housekeeping_service.c
+++ ex2_services/Services/source/housekeeping/housekeeping_service.c
@@ -494,6 +494,7 @@ Result collect_hk_from_devices(All_systems_housekeeping* all_hk_data) {
   #endif /* HYPERION_PANEL_3U */
 
   #ifdef HYPERION_PANEL_2U
+    i2cSetBaudrate(I2C_BUS_REG, 100);
     Hyperion_config3_getHK(&all_hk_data->hyperion_hk);
   #endif /* HYPERION_PANEL_2U */
 
