716076fbbbe94d1767595d0a3a0f3ad444a9d8ba
Nicholas Sorensen <nsorense@ualberta.ca>
2021-11-27 12:44:15 -0700

    Fixing clang formatting on main.c and removing error handle in  HL_sys_link.cmd.

--- source/HL_sys_link.cmd
+++ source/HL_sys_link.cmd
@@ -1,36 +1,36 @@
 /*----------------------------------------------------------------------------*/
 /* sys_link_freeRTOS.cmd                                                      */
 /*                                                                            */
-/* 
-* Copyright (C) 2009-2018 Texas Instruments Incorporated - www.ti.com  
-* 
-* 
-*  Redistribution and use in source and binary forms, with or without 
-*  modification, are permitted provided that the following conditions 
+/*
+* Copyright (C) 2009-2018 Texas Instruments Incorporated - www.ti.com
+*
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
 *  are met:
 *
-*    Redistributions of source code must retain the above copyright 
+*    Redistributions of source code must retain the above copyright
 *    notice, this list of conditions and the following disclaimer.
 *
 *    Redistributions in binary form must reproduce the above copyright
-*    notice, this list of conditions and the following disclaimer in the 
-*    documentation and/or other materials provided with the   
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the
 *    distribution.
 *
 *    Neither the name of Texas Instruments Incorporated nor the names of
 *    its contributors may be used to endorse or promote products derived
 *    from this software without specific prior written permission.
 *
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-*  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-*  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+*  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+*  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
 *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
 *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 */
@@ -46,7 +46,6 @@
 
 /* USER CODE BEGIN (1) */
 #ifdef DO_NOT_COMPILE
-#error "The error was up here!'
 /* USER CODE END */
 
 /*----------------------------------------------------------------------------*/
@@ -55,13 +54,13 @@
 MEMORY
 {
     VECTORS (X)  : origin=0x00000000 length=0x00000020
-    KERNEL  (RX) : origin=0x00000020 length=0x00008000 
+    KERNEL  (RX) : origin=0x00000020 length=0x00008000
     FLASH0  (RX) : origin=0x00008020 length=0x001F7FE0
     FLASH1  (RX) : origin=0x00200000 length=0x00200000
     STACKS  (RW) : origin=0x08000000 length=0x00000800
     KRAM    (RW) : origin=0x08000800 length=0x00000800
     RAM     (RW) : origin=(0x08000800+0x00000800) length=(0x0007f800 - 0x00000800)
-    
+
 /* USER CODE BEGIN (2) */
 	SDRAM  (RWX) : origin=0x80000000 length=0x07FFFFFF//experimental
 /* USER CODE END */
@@ -87,7 +86,7 @@ SECTIONS
     .kernelBSS    : {} > KRAM
     .kernelHEAP   : {} > RAM
     .bss          : {} > RAM
-    .data         : {} > RAM    
+    .data         : {} > RAM
     .sysmem       : {} > RAM
     FEE_TEXT_SECTION align(32) : {} > FLASH0 | FLASH1
     FEE_CONST_SECTION align(32): {} > FLASH0 | FLASH1
