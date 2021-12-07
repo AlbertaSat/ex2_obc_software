de93fcd3ad522545818a45d69999ee8c3949729f
Nicholas Sorensen <nsorense@ualberta.ca>
2021-12-07 10:46:31 -0700

    Adding changes made during testing to improve performance and reduce magic numbers.

--- ex2_hal/hyperion_solar_panel/hardware_interface/source/hyperion.c
+++ ex2_hal/hyperion_solar_panel/hardware_interface/source/hyperion.c
@@ -17,6 +17,8 @@
  * @date    2021-06-04
  */
 #include "hyperion.h"
+#include "os_portmacro.h"
+#include "os_projdefs.h"
 
 
 /**
@@ -284,7 +286,7 @@ void hyperion_config_2_value(config_2_panel_t panel, config_2_channel_type_t cha
         break;
     }
     case CONFIG_3_ADC_TEMP: {
-        adc_init(slave_addr, ADC_CHANNEL_0);
+        adc_init(slave_addr, ADC_CHANNEL_7);
         int8_t *result = (int8_t *)param;
         *result = (int8_t) adc_get_tsense_temp(slave_addr, ADC_VREF);
          break;
@@ -379,7 +381,7 @@ void Hyperion_config1_getHK(Hyperion_HouseKeeping *hyperion_hk) {
     hyperion_config_1_value(CONFIG_1_PANEL_S, CONFIG_1_CHANNEL_VOLT, &hyperion_hk->Star_Voltage);
 
     // Star Dep Voltage
-    hyperion_config_1_value(CONFIG_1_PANEL_S, CONFIG_1_CHANNEL_VOLT, &hyperion_hk->Star_Dep_Voltage);
+    hyperion_config_1_value(CONFIG_1_PANEL_SD, CONFIG_1_CHANNEL_VOLT, &hyperion_hk->Star_Dep_Voltage);
 
     // Zenith Voltage
     hyperion_config_1_value(CONFIG_1_PANEL_Z, CONFIG_1_CHANNEL_VOLT, &hyperion_hk->Zenith_Voltage);
@@ -394,7 +396,7 @@ void Hyperion_config1_getHK(Hyperion_HouseKeeping *hyperion_hk) {
     hyperion_config_1_value(CONFIG_1_PANEL_S, CONFIG_1_CHANNEL_CURR, &hyperion_hk->Star_Current);
 
     // Star Dep Current
-    hyperion_config_1_value(CONFIG_1_PANEL_S, CONFIG_1_CHANNEL_CURR, &hyperion_hk->Star_Dep_Current);
+    hyperion_config_1_value(CONFIG_1_PANEL_SD, CONFIG_1_CHANNEL_CURR, &hyperion_hk->Star_Dep_Current);
 
     // Zenith Current
     hyperion_config_1_value(CONFIG_1_PANEL_Z, CONFIG_1_CHANNEL_CURR, &hyperion_hk->Zenith_Current);
@@ -402,7 +404,7 @@ void Hyperion_config1_getHK(Hyperion_HouseKeeping *hyperion_hk) {
 
 void Hyperion_config3_getHK(Hyperion_HouseKeeping *hyperion_hk) {
     // NADIR TEMP 1
-    hyperion_config_2_value(CONFIG_2_PANEL_NADIR, CONFIG_2_CHANNEL_PD_1, &hyperion_hk->Nadir_Pd1);
+    hyperion_config_2_value(CONFIG_2_PANEL_NADIR, CONFIG_2_CHANNEL_TEMP_1, &hyperion_hk->Nadir_Temp1);
 
     // NADIR Temp Adc
     hyperion_hk->Nadir_Temp_Adc = adc_get_tsense_temp(PANEL_SLAVE_ADDR_NADIR2U, ADC_VREF);
@@ -439,7 +441,7 @@ void Hyperion_config3_getHK(Hyperion_HouseKeeping *hyperion_hk) {
     // Star Dep Adc
     hyperion_config_3_value(CONFIG_3_PANEL_SD2U, CONFIG_3_ADC_TEMP, &hyperion_hk->Star_Dep_Temp_Adc);
 
-    // Zenith Temp 1 2
+    //Zenith Temp 1 2
     hyperion_config_3_value(CONFIG_3_PANEL_Z2U, CONFIG_3_CHANNEL_TEMP_1, &hyperion_hk->Zenith_Temp1);
     hyperion_config_3_value(CONFIG_3_PANEL_Z2U, CONFIG_3_CHANNEL_TEMP_2, &hyperion_hk->Zenith_Temp2);
     hyperion_hk->Zenith_Temp3 = HYPERION_2U_TEMP_PLACEHOLDER;
@@ -485,7 +487,7 @@ void Hyperion_config3_getHK(Hyperion_HouseKeeping *hyperion_hk) {
     hyperion_config_3_value(CONFIG_3_PANEL_S2U, CONFIG_3_CHANNEL_VOLT, &hyperion_hk->Star_Voltage);
 
     // Star Dep 2U Voltage
-    hyperion_config_3_value(CONFIG_3_PANEL_S2U, CONFIG_3_CHANNEL_VOLT, &hyperion_hk->Star_Dep_Voltage);
+    hyperion_config_3_value(CONFIG_3_PANEL_SD2U, CONFIG_3_CHANNEL_VOLT, &hyperion_hk->Star_Dep_Voltage);
 
     // Zenith 2U Voltage
     hyperion_config_3_value(CONFIG_3_PANEL_Z2U, CONFIG_3_CHANNEL_VOLT, &hyperion_hk->Zenith_Voltage);
@@ -500,7 +502,7 @@ void Hyperion_config3_getHK(Hyperion_HouseKeeping *hyperion_hk) {
     hyperion_config_3_value(CONFIG_3_PANEL_S2U, CONFIG_3_CHANNEL_CURR, &hyperion_hk->Star_Current);
 
     // Star Dep 2U Current
-    hyperion_config_3_value(CONFIG_3_PANEL_S2U, CONFIG_3_CHANNEL_CURR, &hyperion_hk->Star_Dep_Current);
+    hyperion_config_3_value(CONFIG_3_PANEL_SD2U, CONFIG_3_CHANNEL_CURR, &hyperion_hk->Star_Dep_Current);
 
     // Zenith 2U Current
     hyperion_config_3_value(CONFIG_3_PANEL_Z2U, CONFIG_3_CHANNEL_CURR, &hyperion_hk->Zenith_Current);
