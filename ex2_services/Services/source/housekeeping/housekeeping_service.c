038df2b2a951473b4d63b8d51b31029e01ab7d56
tganley <thomasjganley@gmail.com>
2021-12-21 14:39:08 -0700

    Revert previous change and add delay to hk collection

--- ex2_services/Services/source/housekeeping/housekeeping_service.c
+++ ex2_services/Services/source/housekeeping/housekeeping_service.c
@@ -480,9 +480,10 @@ Result collect_hk_from_devices(All_systems_housekeeping* all_hk_data) {
 
   #ifndef UHF_IS_STUBBED
     if (xSemaphoreTake(uTransceiver_semaphore, 0) == pdTRUE) {
-        UHF_return UHF_return_code = UHF_getHK(&all_hk_data->UHF_hk);      //UHF get housekeeping
         xSemaphoreGive(uTransceiver_semaphore);
+        UHF_return UHF_return_code = UHF_getHK(&all_hk_data->UHF_hk);      //UHF get housekeeping
     }
+    vTaskDelay(ONE_SECOND);
   #endif /* UHF Housekeeping */
 
   #ifndef SBAND_IS_STUBBED
