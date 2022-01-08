dc8d718ba9da7b8a176fa7badcff510db3cf8a4c
Ron Unrau <rcunrau@ualberta.ca>
2022-01-07 17:25:56 -0700

    main.c: call the adcs tests

--- test/main.c
+++ test/main.c
@@ -1,7 +1,9 @@
 #include "logger/test_logger.h"
+#include "test_adcs_handler.h"
 
 int main() {
     int status = 0;
     status += test_logger();
+    status += test_adcs_handler();
     return status;
 }
