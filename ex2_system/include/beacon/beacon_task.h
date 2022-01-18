2c5c1148af5df3e16705f85c03b784345bb7e11f
Grace Yi <yunqing@ualberta.ca>
2022-01-18 16:15:11 -0700

    delete beacon.h as it is a duplicate of beacon_task.h

--- ex2_system/include/beacon/beacon_task.h
+++ ex2_system/include/beacon/beacon_task.h
@@ -90,67 +90,6 @@ typedef struct __attribute__((packed)) {
     uint8_t log6_code;
 } beacon_packet_2_t;
 
-beacon_packet_1_t beacon_packet_one;
-beacon_packet_2_t beacon_packet_two;
-
-typedef struct __attribute__((packed)) {
-    uint32_t time;
-    uint8_t packet_number;
-} beacon_common_t;
-typedef struct __attribute__((packed)) {
-    beacon_common_t common;
-    //EPS
-    uint8_t eps_mode;
-    uint16_t battery_voltage;
-    uint16_t battery_input_current;
-    uint16_t current_channels[10];
-    uint16_t output_states;
-    uint8_t output_faults[10];
-    uint16_t EPS_boot_count;
-    uint8_t eps_last_reset_reason;
-    //Watchdogs
-    uint16_t gs_wdt;
-    uint8_t obc_wdt;
-    uint16_t gs_wdt_expr;
-    uint16_t obc_wdt_expr;
-    //Temperatures
-    int8_t temps[18];
-    //ADCS
-    int8_t angular_rate;
-    int8_t adcs_control_mode;
-    //UHF
-    uint16_t uhf_uptime;
-    //Payload
-    uint8_t payload_software_version;
-} beacon_packet_1_t;
-typedef struct __attribute__((packed)) {
-    beacon_common_t common;
-    //OBC
-    uint16_t obc_boot_count;
-    uint8_t obc_last_reset_reason;
-    uint8_t obc_mode;
-    uint16_t obc_uptime;
-    uint8_t solar_panel_current;
-    uint16_t mcu_core_current;
-    uint8_t obc_software_version;
-    uint16_t commands_received;
-    uint16_t fec_recovered_packets;
-    uint8_t logged_items_count;
-    //Logged Items
-    uint32_t log1_timestamp;
-    uint8_t log1_code;
-    uint32_t log2_timestamp;
-    uint8_t log2_code;
-    uint32_t log3_timestamp;
-    uint8_t log3_code;
-    uint32_t log4_timestamp;
-    uint8_t log4_code;
-    uint32_t log5_timestamp;
-    uint8_t log5_code;
-    uint32_t log6_timestamp;
-    uint8_t log6_code;
-} beacon_packet_2_t;
-
 extern beacon_packet_1_t beacon_packet_one;
 extern beacon_packet_2_t beacon_packet_two;
 
