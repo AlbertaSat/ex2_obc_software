5392456509f1c55cad397dddc947ce1906fd256c
Daniel Sacro <dbsacro@gmail.com>
2022-02-08 11:26:12 -0700

    Finished general framework

--- ex2_hal/dfgm/hardware_interface/include/dfgm.h
+++ ex2_hal/dfgm/hardware_interface/include/dfgm.h
@@ -1,1 +1,18 @@
-// header for dfgm hardware interface
+// Copyright stuff
+
+#ifndef DFGM_H
+#define DFGM_H
+
+#include "dfgm_handler.h" // Equipment handler
+
+typedef struct __attribute__((packed)) {
+    float attribute1;
+} DFGM_Housekeeping;
+
+STX_return HAL_DFGM_run(int runtime);
+STX_return HAL_DFGM_stop();
+STX_return HAL_DFGM_filterTo10Hz();
+STX_return HAL_DFGM_filterTo1Hz();
+STX_return HAL_DFGM_getHK(DFGM_Housekeeping * DFGM_hk);
+
+#endif /* DFGM_H */
--- ex2_hal/dfgm/hardware_interface/source/dfgm.c
+++ ex2_hal/dfgm/hardware_interface/source/dfgm.c
@@ -1,1 +1,70 @@
-// header for dfgm hardware interface
+// Copyright stuff
+
+#include "dfgm.h"
+#include "dfgm_handler.h"
+
+#include <FreeRTOS.h>
+#include <csp/csp_endian.h>
+#include <os_queue.h>
+
+#include "services.h"
+
+// Static variable for HK?
+
+
+
+STX_return HAL_DFGM_run(int runtime) {
+    STX_return status;
+#ifndef DFGM_IS_STUBBED
+    status = STX_startDFGM(runtime);
+#else
+    status = IS_STUBBED_DFGM;
+#endif
+    // something
+    return status;
+}
+
+STX_return HAL_DFGM_stop() {
+    STX_return status;
+#ifndef DFGM_IS_STUBBED
+    status = STX_stopDFGM();
+#else
+    status = IS_STUBBED_DFGM;
+#endif
+    // something
+    return status;
+}
+
+STX_return HAL_DFGM_filterTo10Hz() {
+    STX_return status;
+#ifndef DFGM_IS_STUBBED
+    status = STX_filter10Hz;
+#else
+    status = IS_STUBBED_DFGM;
+#endif
+    return status;
+}
+
+STX_return HAL_DFGM_filterTo1Hz() {
+    STX_return status;
+#ifndef DFGM_IS_STUBBED
+    status = STX_filter1Hz;
+#else
+    status = IS_STUBBED_DFGM;
+#endif
+    return status;
+}
+
+STX_return HAL_DFGM_getHK(DFGM_Housekeeping * DFGM_hk) {
+    STX_return status;
+#ifndef DFGM_IS_STUBBED
+    status = STX_getHK(dfgm_housekeeping *hk);
+#else
+    // Set HK data
+
+    // Return status
+    status = IS_STUBBED_DFGM;
+#endif
+    // Save to HK buffer ?
+    return status;
+}
