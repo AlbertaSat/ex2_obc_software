40f66033055cfd3ef7728e90432344a8d01ec745
Daniel Sacro <dbsacro@gmail.com>
2022-02-08 11:26:12 -0700

    Finished general framework

--- ex2_hal/dfgm/equipment_handler/include/dfgm_handler.h
+++ ex2_hal/dfgm/equipment_handler/include/dfgm_handler.h
@@ -1,1 +1,38 @@
-// Header for dfgm_handler
+// Copyright stuff
+
+
+#ifndef DFGM_HANDLER_H
+#define DFGM_HANDLER_H
+
+// Headers
+#include "system.h"
+
+typedef enum {
+    DFGM_SUCCESS = 0,
+
+    DFGM_BAD_READ = 1,
+    DFGM_BAD_WRITE = 2,
+
+    IS_STUBBED_DFGM = 0;
+} STX_return;
+
+// Macros for conversions from converter and filter header and source files (QUEUE_DEPTH, etc.)
+
+// Structs from converter and filter - dfgm_data_tuple_t, dfgm_packet_t, dfgm_data_t, HK data struct etc.
+// SECOND + dfgm_1Hz_file_t combo? should include timestamps, 100Hz data, and filtered data
+
+
+
+
+typedef struct {
+    float attribute1;
+} dfgm_housekeeping;
+
+
+
+
+STX_return STX_startDFGM(int runtime);
+STX_return STX_stopDFGM();
+STX_return STX_filterTo10Hz();
+STX_return STX_filterTo1Hz();
+STX_return STX_getHK(dfgm_housekeeping *hk);
--- ex2_hal/dfgm/equipment_handler/source/dfgm_handler.c
+++ ex2_hal/dfgm/equipment_handler/source/dfgm_handler.c
@@ -1,1 +1,34 @@
-// Source code for dfgm_handler
+// Copyright stuff
+
+
+// + Other headers used in converter.c and filter.c (Reliance Edge, etc.)
+#include "dfgm_handler.c"
+
+// Static values (dfgm_buffer, dfgmQueue, etc.)
+
+
+// HKScales and HKOffsets array
+
+
+// Coefficients for 1 Hz filter
+
+
+// Coefficients for 10 Hz filter
+
+
+// SECOND buffer and SECOND sptr (second pointer)
+// SECOND is a struct
+
+
+// Helper functions that dfgm_converter.c and dfgm_filter.c used
+
+
+
+
+
+//Actual function implementations
+//STX_return STX_startDFGM(int runtime);
+//STX_return STX_stopDFGM();
+//STX_return STX_filterTo10Hz();
+//STX_return STX_filterTo1Hz();
+//STX_return STX_getHK(dfgm_housekeeping *hk);
