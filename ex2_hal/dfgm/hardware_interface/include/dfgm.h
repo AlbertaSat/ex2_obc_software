cfce6ded45676d88eb5dfb14c541ffc6bce985a5
Daniel Sacro <dbsacro@gmail.com>
2022-02-13 15:26:59 -0700

    Finished subservice development, requires testing

--- ex2_hal/dfgm/hardware_interface/include/dfgm.h
+++ ex2_hal/dfgm/hardware_interface/include/dfgm.h
@@ -1,18 +1,53 @@
-// Copyright stuff
+/*
+ * Copyright (C) 2015  University of Alberta
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+/**
+ * @file
+ * @author
+ * @date
+ */
 
 #ifndef DFGM_H
 #define DFGM_H
 
 #include "dfgm_handler.h" // Equipment handler
+#include <stdint.h>
+#include <time.h>
 
 typedef struct __attribute__((packed)) {
-    float attribute1;
+    float coreVoltage;
+    float sensorTemp;
+    float refTemp;
+    float boardTemp;
+    float posRailVoltage;
+    float inputVoltage;
+    float refVoltage;
+    float inputCurrent;
+    float reserved1;
+    float reserved2;
+    float reserved3;
+    float reserved4;
 } DFGM_Housekeeping;
 
-STX_return HAL_DFGM_run(int runtime);
+typedef struct __attribute__((packed)) {
+    uint8_t filterMode;
+    uint32_t startTime;
+    uint32_t endTime;
+} DFGM_Filter_Settings;
+
+STX_return HAL_DFGM_run(int32_t givenRuntime);
 STX_return HAL_DFGM_stop();
-STX_return HAL_DFGM_filterTo10Hz();
-STX_return HAL_DFGM_filterTo1Hz();
-STX_return HAL_DFGM_getHK(DFGM_Housekeeping * DFGM_hk);
+STX_return HAL_DFGM_filter(DFGM_Filter_Settings *DFGM_filterSettings);
+STX_return HAL_DFGM_getHK(DFGM_Housekeeping *DFGM_hk);
 
 #endif /* DFGM_H */
--- ex2_hal/dfgm/hardware_interface/source/dfgm.c
+++ ex2_hal/dfgm/hardware_interface/source/dfgm.c
@@ -1,70 +1,103 @@
-// Copyright stuff
+/*
+ * Copyright (C) 2015  University of Alberta
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+/**
+ * @file
+ * @author
+ * @date
+ */
 
 #include "dfgm.h"
-#include "dfgm_handler.h"
 
 #include <FreeRTOS.h>
 #include <csp/csp_endian.h>
 #include <os_queue.h>
+#include <stdint.h>
 
 #include "services.h"
 
-// Static variable for HK?
-
-
-
-STX_return HAL_DFGM_run(int runtime) {
+STX_return HAL_DFGM_run(int32_t givenRuntime) {
     STX_return status;
 #ifndef DFGM_IS_STUBBED
+    // If DFGM is connected, run
+    int runtime = (int) givenRuntime;
     status = STX_startDFGM(runtime);
 #else
+    // If DFGM is not connected, can't run
     status = IS_STUBBED_DFGM;
 #endif
-    // something
     return status;
 }
 
 STX_return HAL_DFGM_stop() {
     STX_return status;
 #ifndef DFGM_IS_STUBBED
+    // DFGM is connected
     status = STX_stopDFGM();
 #else
-    status = IS_STUBBED_DFGM;
-#endif
-    // something
-    return status;
-}
-
-STX_return HAL_DFGM_filterTo10Hz() {
-    STX_return status;
-#ifndef DFGM_IS_STUBBED
-    status = STX_filter10Hz;
-#else
+    // DFGM is not connected
     status = IS_STUBBED_DFGM;
 #endif
     return status;
 }
 
-STX_return HAL_DFGM_filterTo1Hz() {
+STX_return HAL_DFGM_filter(DFGM_Filter_Settings *DFGM_filterSettings) {
     STX_return status;
+    dfgm_filter_settings filterSettings;
 #ifndef DFGM_IS_STUBBED
-    status = STX_filter1Hz;
+    filterSettings.filterMode = (int) DFGM_filterSettings->filterMode;
+    filterSettings.startTime = DFGM_filterSettings->startTime;
+    filterSettings.endTime = DFGM_filterSettings->endTime;
+    status = STX_filterDFGM(&filterSettings);
 #else
+    // DFGM not connected
     status = IS_STUBBED_DFGM;
 #endif
     return status;
 }
 
-STX_return HAL_DFGM_getHK(DFGM_Housekeeping * DFGM_hk) {
+STX_return HAL_DFGM_getHK(DFGM_Housekeeping *DFGM_hk) {
     STX_return status;
+    dfgm_housekeeping hk;
 #ifndef DFGM_IS_STUBBED
-    status = STX_getHK(dfgm_housekeeping *hk);
+    status = STX_getDFGMHK(&hk);
+    DFGM_hk->coreVoltage = hk.coreVoltage;
+    DFGM_hk->sensorTemp = hk.sensorTemp;
+    DFGM_hk->refTemp = hk.refTemp;
+    DFGM_hk->boardTemp = hk.boardTemp;
+    DFGM_hk->posRailVoltage = hk.posRailVoltage;
+    DFGM_hk->inputVoltage = hk.inputVoltage;
+    DFGM_hk->refVoltage = hk.refVoltage;
+    DFGM_hk->inputCurrent = hk.inputCurrent;
+    DFGM_hk->reserved1 = hk.reserved1;
+    DFGM_hk->reserved2 = hk.reserved2;
+    DFGM_hk->reserved3 = hk.reserved3;
+    DFGM_hk->reserved4 = hk.reserved4;
 #else
-    // Set HK data
-
-    // Return status
     status = IS_STUBBED_DFGM;
+    // Default values to signify DFGM is not connected
+    DFGM_hk->coreVoltage = 1;
+    DFGM_hk->sensorTemp = 1;
+    DFGM_hk->refTemp = 1;
+    DFGM_hk->boardTemp = 1;
+    DFGM_hk->posRailVoltage = 1;
+    DFGM_hk->inputVoltage = 1;
+    DFGM_hk->refVoltage = 1;
+    DFGM_hk->inputCurrent = 1;
+    DFGM_hk->reserved1 = 1;
+    DFGM_hk->reserved2 = 1;
+    DFGM_hk->reserved3 = 1;
+    DFGM_hk->reserved4 = 1;
 #endif
-    // Save to HK buffer ?
     return status;
 }
