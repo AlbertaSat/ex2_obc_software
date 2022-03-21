5397c6afcd05583b000b7f875e3502855011acb0
Ron Unrau <rcunrau@ualberta.ca>
2022-03-21 09:31:55 -0600

    Build script fixes

--- build-compile.sh
+++ build-compile.sh
@@ -5,7 +5,7 @@ set -e
 BUILD_TOOL_ROOT=${BUILD_TOOL_ROOT:-"$HOME/ti/ccs1040"}
 
 # Builds the ex2_obc_software project using the specified configuration (the
-active configuration if omitted) and workspace.
+# active configuration if omitted) and workspace.
 usage() {
     echo "Usage: $0 [-c <configuration> ] [-w <workspace>]"
     exit 1
@@ -29,7 +29,7 @@ while getopts ":c:w:" o; do
 done
 
 echo "Building $configuration from workspace $workspace"
-${TOOL_ROOT}/ccs/eclipse/eclipse -noSplash -data "$workspace" -application com.ti.ccstudio.apps.buildProject -ccs.projects ex2_obc_software "$configuration" -ccs.listErrors
+${BUILD_TOOL_ROOT}/ccs/eclipse/eclipse -noSplash -data "$workspace" -application com.ti.ccstudio.apps.buildProject -ccs.projects ex2_obc_software "$configuration" -ccs.listErrors
 
 exit 0
 
