f71b04281b8c9b5194f5270b71ef316d1937dcc0
Daniel Sacro <dbsacro@gmail.com>
2022-04-14 12:56:26 -0600

    Fixed a lot of errors by renaming files, variables, and functions

--- ex2_services/Services/source/ft_2u_payload/ft_2u_payload_service.c
+++ ex2_services/Services/source/ft_2u_payload/ft_2u_payload_service.c
@@ -12,25 +12,25 @@
  * GNU General Public License for more details.
  */
 /**
- * @file
+ * @file ft_2u_payload_service.c
  * @author Daniel Sacro
  * @date
  */
 
-#include "2u_payload_file_transferring/2u_payload_file_transferring_service.h"
+#include "ft_2u_payload/ft_2u_payload_service.h"
 
 #include <FreeRTOS.h>
 #include <csp/csp.h>
 #include <csp/csp_endian.h>
 #include <main/system.h>
 
-#include "2u_payload_file_transfer.h"
+#include "ft_2u_payload.h"
 #include "services.h"
 #include "task_manager/task_manager.h"
 #include "util/service_utilities.h"
 
 // TODO - Rename all variables and function names. They cannot begin with a digit
-SAT_returnState 2U_payload_FT_service_app(csp_packet_t *packet);
+SAT_returnState FT_2U_payload_service_app(csp_packet_t *packet);
 
 static uint32_t svc_wdt_counter = 0;
 static uint32_t get_svc_wdt_counter() { return svc_wdt_counter; }
@@ -44,7 +44,7 @@ static uint32_t get_svc_wdt_counter() { return svc_wdt_counter; }
  * @param void* param
  * @return None
  */
-void 2U_payload_FT_service(void *param) {
+void FT_2U_payload_service(void *param) {
     // socket initialization
     csp_socket_t *sock;
     sock = csp_socket(CSP_SO_RDPREQ);
@@ -66,7 +66,7 @@ void 2U_payload_FT_service(void *param) {
 
         // read and process packets
         while ((packet = csp_read(conn, 50)) != NULL) {
-            if (2U_payload_FT_service_app(packet) != SATR_OK) {
+            if (FT_2U_payload_service_app(packet) != SATR_OK) {
                 // something went wrong in the subservice
                 csp_buffer_free(packet);
             } else {
@@ -90,12 +90,12 @@ void 2U_payload_FT_service(void *param) {
  * @return SAT_returnState
  *      Success report
  */
-SAT_returnState start_2U_payload_FT_service(void) {
+SAT_returnState start_FT_2U_payload_service(void) {
     TaskHandle_t svc_tsk;
     taskFunctions svc_funcs = {0};
     svc_funcs.getCounterFunction = get_svc_wdt_counter;
 
-    if (xTaskCreate((TaskFunction_t)2U_payload_FT_service, "2U_payload_FT_service", 1024, NULL,
+    if (xTaskCreate((TaskFunction_t)FT_2U_payload_service, "FT_2U_payload_service", 1024, NULL,
                     NORMAL_SERVICE_PRIO, &svc_tsk) != pdPASS) {
         ex2_log("FAILED TO CREATE TASK start_2U_payload_FT_service\n");
         return SATR_ERROR;
@@ -116,13 +116,13 @@ SAT_returnState start_2U_payload_FT_service(void) {
  * @return SAT_returnState
  *      Success or failure
  */
-SAT_returnState 2U_payload_FT_service_app(csp_packet_t *packet) {
+SAT_returnState FT_2U_payload_service_app(csp_packet_t *packet) {
     uint8_t ser_subtype = (uint8_t)packet->data[SUBSERVICE_BYTE];
     int8_t status;
     SAT_returnState return_state = SATR_OK; // OK until an error is encountered
 
     switch (ser_subtype) {
-    case 2U_PAYLOAD_DOWNLINK: {
+    case FT_2U_PAYLOAD_DOWNLINK: {
         // Get filename
 
         // Prepare to send file data
@@ -134,7 +134,7 @@ SAT_returnState 2U_payload_FT_service_app(csp_packet_t *packet) {
         break;
     }
 
-    case 2U_PAYLOAD_UPLINK: {
+    case FT_2U_PAYLOAD_UPLINK: {
         // Get filename
         //cnv8_32(&packet->data[IN_DATA_BYTE], &givenRuntime);
 
@@ -147,7 +147,7 @@ SAT_returnState 2U_payload_FT_service_app(csp_packet_t *packet) {
         break;
     }
 
-    case 2U_PAYLOAD_STOP_FT: {
+    case FT_2U_PAYLOAD_STOP_FT: {
         // Tell OBC to stop processing or sending file data
         //status = HAL_2U_PAYLOAD_stopFileTransfer();
 
@@ -157,7 +157,7 @@ SAT_returnState 2U_payload_FT_service_app(csp_packet_t *packet) {
         break;
     }
 
-    case 2U_PAYLOAD_SEND_BYTES: {
+    case FT_2U_PAYLOAD_SEND_BYTES: {
         // Tell OBC to send file data to GS
         //status = HAL_2U_PAYLOAD_sendDataBytes();
 
@@ -167,14 +167,14 @@ SAT_returnState 2U_payload_FT_service_app(csp_packet_t *packet) {
         break;
     }
 
-    case 2U_PAYLOAD_PROCESS_BYTES: {
+    case FT_2U_PAYLOAD_PROCESS_BYTES: {
         // Receive N bytes of file data from the GS and process it
         // status = HAL_2U_PAYLOAD_receiveDataBytes();
 
         // Return success report (saying ready to receive or abort FT)
         memcpy(&packet->data[STATUS_BYTE], &status, sizeof(int8_t));
-        memcpy(&packet->data[OUT_DATA_BYTE], &HK, sizeof(HK));
-        set_packet_length(packet, sizeof(int8_t) + sizeof(HK) + 1);
+//        memcpy(&packet->data[OUT_DATA_BYTE], &HK, sizeof(HK));
+//        set_packet_length(packet, sizeof(int8_t) + sizeof(HK) + 1);
         break;
     }
 
