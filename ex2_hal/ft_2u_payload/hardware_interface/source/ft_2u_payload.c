57f74a32609e56867dc27f864ce0c4cdb75517fe
Daniel Sacro <dbsacro@gmail.com>
2022-04-14 15:49:05 -0600

    Finished writing EH and HAL functions

--- ex2_hal/ft_2u_payload/hardware_interface/source/ft_2u_payload.c
+++ ex2_hal/ft_2u_payload/hardware_interface/source/ft_2u_payload.c
@@ -19,30 +19,59 @@
 
 #include "ft_2u_payload.h"
 #include "services.h"
+#include <string.h>
 
 
-FT_2U_payload_return HAL_FT_2U_PAYLOAD_getFile() {
+FT_2U_payload_return HAL_FT_2U_PAYLOAD_getFile(char * filename) {
     FT_2U_payload_return status;
-    // Call EH
+#ifndef PAYLOAD_2U_IS_STUBBED
+    status = FT_2U_payload_getFile(filename);
+#else
+    status = IS_STUBBED_2U_PAYLOAD;
+#endif
     return status;
 }
 
-FT_2U_payload_return HAL_FT_2U_PAYLOAD_putFile() {
+FT_2U_payload_return HAL_FT_2U_PAYLOAD_putFile(char * filename) {
     FT_2U_payload_return status;
+#ifndef PAYLOAD_2U_IS_STUBBED
+    status = FT_2U_payload_putFile(filename);
+#else
+    status = IS_STUBBED_2U_PAYLOAD;
+#endif
     return status;
 }
 
 FT_2U_payload_return HAL_FT_2U_PAYLOAD_stopFileTransfer() {
     FT_2U_payload_return status;
+#ifndef PAYLOAD_2U_IS_STUBBED
+    status = FT_2U_payload_stopFileTransfer();
+#else
+    status = IS_STUBBED_2U_PAYLOAD;
+#endif
     return status;
 }
 
-FT_2U_payload_return HAL_FT_2U_PAYLOAD_sendDataBytes() {
+FT_2U_payload_return HAL_FT_2U_PAYLOAD_sendDataBytes(FT_2U_PAYLOAD_filePacket * outgoingPacket) {
     FT_2U_payload_return status;
+#ifndef PAYLOAD_2U_IS_STUBBED
+    filePacket outgoingPkt = {0};
+    status = FT_2U_payload_sendDataBytes(&outgoingPkt);
+    memcpy(outgoingPacket, &outgoingPkt, sizeof(outgoingPkt));
+#else
+    status = IS_STUBBED_2U_PAYLOAD;
+#endif
     return status;
 }
 
-FT_2U_payload_return HAL_FT_2U_PAYLOAD_receiveDataBytes() {
+FT_2U_payload_return HAL_FT_2U_PAYLOAD_receiveDataBytes(FT_2U_PAYLOAD_filePacket * incomingPacket) {
     FT_2U_payload_return status;
+#ifndef PAYLOAD_2U_IS_STUBBED
+    filePacket incomingPkt = {0};
+    memcpy(&incomingPkt, incomingPacket, sizeof(incomingPkt));
+    status = FT_2U_payload_receiveDataBytes(&incomingPkt);
+#else
+    status = IS_STUBBED_2U_PAYLOAD;
+#endif
     return status;
 }
