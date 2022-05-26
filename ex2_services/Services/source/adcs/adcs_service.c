65d0166b0b95f8d137929ab6bb32f2c1dcce304f
Robert <taylorr@ualberta.ca>
2022-05-26 12:00:22 -0600

    Finish migrating struct xyz to struct LLH

--- ex2_services/Services/source/adcs/adcs_service.c
+++ ex2_services/Services/source/adcs/adcs_service.c
@@ -846,11 +846,11 @@ SAT_returnState adcs_service_app(csp_packet_t *packet) {
     }
 
     case ADCS_GET_SAT_POS_LLH: {
-        xyz target;
+        LLH target;
         status = HAL_ADCS_get_sat_pos_LLH(&target);
         memcpy(&packet->data[STATUS_BYTE], &status, sizeof(int8_t));
-        memcpy(&packet->data[OUT_DATA_BYTE], &target, sizeof(xyz));
-        set_packet_length(packet, sizeof(xyz) + sizeof(int8_t) + 1);
+        memcpy(&packet->data[OUT_DATA_BYTE], &target, sizeof(LLH));
+        set_packet_length(packet, sizeof(LLH) + sizeof(int8_t) + 1);
         break;
     }
 
