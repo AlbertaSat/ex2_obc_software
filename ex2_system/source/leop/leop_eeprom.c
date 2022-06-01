728e430415fce4c61a31987daeaabe510a764c85
Robert <taylorr@ualberta.ca>
2022-06-01 16:54:42 -0600

    Add error checking to eeprom

--- ex2_system/source/leop/leop_eeprom.c
+++ ex2_system/source/leop/leop_eeprom.c
@@ -8,10 +8,22 @@
 #include "leop_eeprom.h"
 #include "F021.h"
 
+typedef struct __attribute__((packed)){
+    uint32_t exists_flag;
+    bool status;
+} leop_status_t;
+
 bool eeprom_get_leop_status() {
-    bool out;
-    if (eeprom_read(&out, LEOP_INFO_BLOCKNUMBER, sizeof(out)) == Fapi_Status_Success) {
-        return true;
+    leop_status_t state = {0};
+    if (eeprom_read(&state, LEOP_INFO_BLOCKNUMBER, sizeof(state)) == Fapi_Status_Success) {
+        if (state.exists_flag == EXISTS_FLAG) {
+            return state.status;
+        } else {
+            memset(&state, 0, sizeof(state));
+            state.exists_flag = EXISTS_FLAG;
+            eeprom_write(&state, LEOP_INFO_BLOCKNUMBER, sizeof(state));
+            return state.status;
+        }
     }
     else {
         return false;
@@ -19,7 +31,9 @@ bool eeprom_get_leop_status() {
 }
 
 bool eeprom_set_leop_status() {
-    bool leop_status = true;
-    eeprom_write(&leop_status, LEOP_INFO_BLOCKNUMBER, sizeof(leop_status));
+    leop_status_t state = {0};
+    state.exists_flag = EXISTS_FLAG;
+    state.status = true;
+    eeprom_write(&state, LEOP_INFO_BLOCKNUMBER, sizeof(state));
     return true;
 }
