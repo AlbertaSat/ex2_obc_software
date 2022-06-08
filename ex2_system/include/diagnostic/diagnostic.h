5db3f2eefcf510522ef58bb4f1273363477613ff
tganley <58645798+tganley@users.noreply.github.com>
2022-06-08 07:39:09 -0600

    Convert ms to ticks explicitly

--- ex2_system/include/diagnostic/diagnostic.h
+++ ex2_system/include/diagnostic/diagnostic.h
@@ -30,9 +30,9 @@ TickType_t get_sband_watchdog_delay(void);
 TickType_t get_charon_watchdog_delay(void);
 TickType_t get_adcs_watchdog_delay(void);
 
-SAT_returnState set_uhf_watchdog_delay(const TickType_t delay);
-SAT_returnState set_sband_watchdog_delay(const TickType_t delay);
-SAT_returnState set_charon_watchdog_delay(const TickType_t delay);
-SAT_returnState set_adcs_watchdog_delay(const TickType_t delay);
+SAT_returnState set_uhf_watchdog_delay(const unsigned int ms_delay);
+SAT_returnState set_sband_watchdog_delay(const unsigned int ms_delay);
+SAT_returnState set_charon_watchdog_delay(const unsigned int ms_delay);
+SAT_returnState set_adcs_watchdog_delay(const unsigned int ms_delay);
 
 #endif /* EX2_SYSTEM_INCLUDE_DIAGNOSTIC_DIAGNOSTIC_H_ */
--- ex2_system/source/diagnostic/diagnostic.c
+++ ex2_system/source/diagnostic/diagnostic.c
@@ -364,12 +364,12 @@ SAT_returnState set_uhf_watchdog_delay(const TickType_t delay) {
 #endif
 }
 
-SAT_returnState set_sband_watchdog_delay(const TickType_t delay) {
+SAT_returnState set_sband_watchdog_delay(const unsigned int delay) {
 #ifdef SBAND_IS_STUBBED
     return SATR_OK;
 #else
     if (xSemaphoreTake(sband_watchdog_mtx, mutex_timeout) == pdPASS) {
-        sband_prv_watchdog_delay = delay;
+        sband_prv_watchdog_delay = pdMS_TO_TICKS(delay);
         xSemaphoreGive(sband_watchdog_mtx);
         return SATR_OK;
     }
@@ -377,12 +377,12 @@ SAT_returnState set_sband_watchdog_delay(const TickType_t delay) {
 #endif
 }
 
-SAT_returnState set_charon_watchdog_delay(const TickType_t delay) {
+SAT_returnState set_charon_watchdog_delay(const unsigned int delay) {
 #ifdef CHARON_IS_STUBBED
     return SATR_OK;
 #else
     if (xSemaphoreTake(charon_watchdog_mtx, mutex_timeout) == pdPASS) {
-        charon_prv_watchdog_delay = delay;
+        charon_prv_watchdog_delay = pdMS_TO_TICKS(delay);
         xSemaphoreGive(charon_watchdog_mtx);
         return SATR_OK;
     }
@@ -390,12 +390,12 @@ SAT_returnState set_charon_watchdog_delay(const TickType_t delay) {
 #endif
 }
 
-SAT_returnState set_adcs_watchdog_delay(const TickType_t delay) {
+SAT_returnState set_adcs_watchdog_delay(const unsigned int delay) {
 #ifdef ADCS_IS_STUBBED
     return SATR_OK;
 #else
     if (xSemaphoreTake(adcs_watchdog_mtx, mutex_timeout) == pdPASS) {
-        adcs_prv_watchdog_delay = delay;
+        adcs_prv_watchdog_delay = pdMS_TO_TICKS(delay);
         xSemaphoreGive(adcs_watchdog_mtx);
         return SATR_OK;
     }
