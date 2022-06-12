44687095b0133719a20efe7833205bb24995ed39
Jenish Patel <jenish@ualberta.ca>
2022-06-12 17:32:53 -0600

    reverted main.c to original state

--- main/main.c
+++ main/main.c
@@ -63,17 +63,13 @@
 #include "dfgm.h"
 #include "leop.h"
 #include "adcs.h"
-#include "iris_bootloader_cmds.h"
 #include "deployablescontrol.h"
 #include "test_sdr.h"
 #include <csp/interfaces/csp_if_sdr.h>
 #include "printf.h"
-
-
 #include "csp/crypto/csp_hmac.h"
 #include "crypto.h"
 #include "csp_debug_wrapper.h"
-
 
 //#define CSP_USE_SDR
 #define CSP_USE_KISS
@@ -194,63 +190,12 @@ void flatsat_test(void *pvParameters) {
 }
 #endif
 
-void iris_i2c_test(void *pvParameters) {
-    iris_i2c_init();
-    uint32_t flash_addr = FLASH_MEM_BASE_ADDR;
-    uint8_t num_bytes = 0x10;
-    uint8_t *buffer;
-    const char * filepath = "/home/jenish/Desktop/ex2_Iris_MCU_Software/Debug/ex2_Iris_MCU_Software.bin";
-    buffer = (uint8_t*) calloc(128, sizeof(uint8_t));
-
-    FILE *fptr;
-    fptr = fopen(filepath, "rb");
-
-    if (fptr == NULL) {
-        return NULL;
-    }
-
-    uint16_t fsize = get_file_size(fptr);
-    uint16_t num_pages = get_num_pages(fsize);
-
-    for (;;) {
-        iris_pre_sequence();
-//        iris_mass_erase_flash();
-//        vTaskDelay(500);
-//        iris_post_sequence();
-//
-//        iris_pre_sequence();
-        for (int page = 0; page < num_pages; page++) {
-            iris_erase_page(page);
-            read_bin_file(fptr, buffer);
-            iris_write_page(flash_addr, buffer);
-            memset(buffer, 0, FLASH_MEM_PAGE_SIZE);
-
-            flash_addr += FLASH_MEM_PAGE_SIZE;
-        }
-
-        flash_addr = FLASH_MEM_BASE_ADDR;
-
-        //iris_write_page(flash_addr);
-        //iris_erase_page(0);
-        //iris_mass_erase_flash();
-        //iris_check_bootloader_version();
-        iris_go_to(flash_addr);
-        iris_post_sequence();
-
-        free(buffer);
-        fclose(fptr);
-        //gio_test();
-    }
-}
-
-
 int ex2_main(void) {
     _enable_IRQ_interrupt_(); // enable inturrupts
     InitIO();
     for (int i = 0; i < 1000000; i++)
         ;
     xTaskCreate(ex2_init, "init", INIT_STACK_SIZE, NULL, INIT_PRIO, NULL);
-    xTaskCreate(iris_i2c_test, "iris_test", INIT_STACK_SIZE, NULL, INIT_PRIO, NULL);
 
     /* Start FreeRTOS! */
     vTaskStartScheduler();
@@ -258,8 +203,6 @@ int ex2_main(void) {
     for (;;)
         ; // Scheduler didn't start
 }
-
-
 
 /**
  * Initialize service and system tasks
