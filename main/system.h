aee3df7688949a70ab553359d002bf46d24988d2
Jenish Patel <jenish@ualberta.ca>
2022-06-13 15:53:00 -0600

    Defined IRIS_I2C in system.h

--- main/system.h
+++ main/system.h
@@ -89,7 +89,7 @@
 #else // These values are expected to be adjusted based on what the developer is working on
 #define IRIS_CONFIG_SPI spiREG4 // SPI1
 #define IRIS_SPI spiREG5        // SPI3
-#define IRIS_I2C i2cREG1        // TODO: TESTING PURPOSE
+#define IRIS_I2C i2cREG1        // I2C1
 #define SBAND_SPI spiREG3       // SPI2
 #define SD_SPI spiREG1          //?
 #endif
