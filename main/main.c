605baebda14e3571cd9b368af786f325e22dea05
Jenish Patel <jenish@ualberta.ca>
2022-06-22 15:43:01 -0600

    Use spiREG4 for athena-iris flatsat testing

--- main/main.c
+++ main/main.c
@@ -214,7 +214,7 @@ void iris_spi_test(void * pvParameters) {
     for(;;) {
 //        spi_write_read(1, &tx_data, rx_data);
 //        vTaskDelay(pdMS_TO_TICKS( 1000UL ));
-//        iris_take_pic();
+        iris_take_pic();
 //          iris_get_image_length(&image_length);
 //          iris_transfer_image(image_length);
 //        iris_get_image_count(&image_count);
