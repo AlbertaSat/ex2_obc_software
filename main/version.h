e6c497a24f26328dd0e770c44512c5cab3065b0c
tganley <58645798+tganley@users.noreply.github.com>
2022-06-28 16:45:31 -0600

    Delete version.h

--- main/version.h
+++
@@ -1,6 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-
-static const char *ex2_version = "tganley/feature/adcs-file-download: 2022-05-27.tganley.161ec1ad855d1835c1f5e5c3186a6b07f4c2c59d";
-
-#endif //VERSION_H
