86dcd09295d30963c93003d350de37c68d4210a3
Jenish Patel <jenish@ualberta.ca>
2022-07-04 08:15:58 -0600

    Updated iris sub-services

--- ex2_hal/iris/hardware_interface/source/iris.c
+++ ex2_hal/iris/hardware_interface/source/iris.c
@@ -30,8 +30,17 @@
  * @brief
  *   Initialize low-level spi driver settings
  **/
-void iris_init() {
+Iris_HAL_return iris_init() {
     iris_spi_init();
+
+    IRIS_BOOT_LOW();
+
+    IRIS_nRST_LOW();
+    vTaskDelay(1);
+    IRIS_nRST_HIGH();
+
+    // TODO: Add quick iris loopback test
+    return IRIS_HAL_OK;
 }
 
 /**
