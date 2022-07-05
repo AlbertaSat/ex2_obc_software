b1689a962c1897e82ef48c11ca4983e90134be26
Jenish Patel <jenish@ualberta.ca>
2022-07-04 19:17:42 -0600

    Save proper VIS image

--- main/main.c
+++ main/main.c
@@ -189,12 +189,13 @@ void iris_spi_test(void *pvParameters) {
         iris_take_pic();
         vTaskDelay(2000);
 
+        iris_take_pic();
+        vTaskDelay(2000);
         iris_get_image_length(&image_length);
         iris_transfer_image(image_length);
 
+        //iris_toggle_sensor_idle(0);
         iris_get_housekeeping(&hk_data);
-
-        // iris_update_sensor_i2c_reg();
     }
     // vTaskDelay(pdMS_TO_TICKS( 1000UL ));
 }
