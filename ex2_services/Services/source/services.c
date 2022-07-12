32c8edda54fb214875b9cf71c0e77842001b0fb0
Grace Yi <yunqing@ualberta.ca>
2022-07-12 17:29:14 -0600

    remove start_logger_service from running twice

--- ex2_services/Services/source/services.c
+++ ex2_services/Services/source/services.c
@@ -56,19 +56,11 @@ SAT_returnState start_service_server(void) {
         pdPASS) {
         return SATR_ERROR;
     }
-    services start_service_function[] = {&start_cli_service,
-                                         &start_communication_service,
-                                         &start_time_management_service,
-                                         &start_scheduler_service,
-                                         &start_housekeeping_service,
-                                         &start_general_service,
-                                         &start_logger_service,
-                                         &start_logger_service,
-                                         &start_dfgm_service,
-                                         &start_adcs_service,
-                                         &start_FTP_service,
-                                         &start_ns_payload_service,
-                                         NULL};
+    services start_service_function[] = {
+        &start_cli_service,       &start_communication_service, &start_time_management_service,
+        &start_scheduler_service, &start_housekeeping_service,  &start_general_service,
+        &start_logger_service,    &start_dfgm_service,          &start_adcs_service,
+        &start_FTP_service,       &start_ns_payload_service,    NULL};
 
     int number_of_cmds = ((sizeof(start_service_function) - 1) / sizeof(services));
 
