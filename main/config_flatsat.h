6b5fce355c0da3e2b4003be4b774a83147a87774
Jenish Patel <jenish@ualberta.ca>
2022-07-20 15:39:53 -0600

    Added various config files [skip actions]

--- main/config_flatsat.h
+++ main/config_flatsat.h
@@ -1,21 +1,21 @@
 /*
  * config.h
  *
- *  Created on: Jul. 2, 2022
- *      Author: Robert
+ *  Created on: Jul. 20, 2022
+ *      Author: Jenish Patel
  */
 
-#ifndef CONFIG_H_
-#define CONFIG_H_
+#ifndef CONFIG_FLATSAT_H_
+#define CONFIG_FLATSAT_H_
 
 #define SYSTEM_APP_ID _OBC_APP_ID_
 
-#define IS_ATHENA 0
+#define IS_ATHENA 1
 #define IS_ATHENA_V2 0
 
 #define IS_SATELLITE 1
 #if IS_SATELLITE == 1
-#define IS_FLATSAT 0
+#define IS_FLATSAT 1
 #define IS_EXALTA2 1
 #define IS_YUKONSAT 0
 #define IS_AURORASAT 0
@@ -32,22 +32,22 @@
 #define SD_CARD_REFORMAT 0
 #endif
 
-#define ATHENA_IS_STUBBED 1
-#define UHF_IS_STUBBED 1
-#define ADCS_IS_STUBBED 1
-#define SBAND_IS_STUBBED 1
-#define EPS_IS_STUBBED 1
-#define HYPERION_IS_STUBBED 1
-#define CHARON_IS_STUBBED 1
-#define DFGM_IS_STUBBED 1
-#define WATCHDOG_IS_STUBBED 1
+#define ATHENA_IS_STUBBED 0
+#define UHF_IS_STUBBED 0
+#define ADCS_IS_STUBBED 0
+#define SBAND_IS_STUBBED 0
+#define EPS_IS_STUBBED 0
+#define HYPERION_IS_STUBBED 0
+#define CHARON_IS_STUBBED 0
+#define DFGM_IS_STUBBED 0
+#define WATCHDOG_IS_STUBBED 0
 #define PAYLOAD_IS_STUBBED 0
 
 #define HYPERION_PANEL_3U 0
-#define HYPERION_PANEL_2U 0
-#define HYPERION_PANEL_2U_LIMITED 0
+#define HYPERION_PANEL_2U 1
+#define HYPERION_PANEL_2U_LIMITED 1
 
-#define IS_SN0072_EPS 0
+#define IS_SN0072_EPS 1
 
 #define UHF_USE_I2C_CMDS 0
 
@@ -56,14 +56,14 @@
 #define ADCS_MAG_FLIGHT_CONFIG 0
 
 #define CSP_FREERTOS 1
-#define CSP_USE_SDR 0
-#define CSP_USE_KISS 1
+#define CSP_USE_SDR 1
+#define CSP_USE_KISS 0
 
 /* Define SDR_NO_CSP==0 to use CSP for SDR */
 #define SDR_NO_CSP 0
 #define OS_FREERTOS
 
-#define FLATSAT_TEST 0
+#define FLATSAT_TEST 1
 
 /**
  * SANITY CHECKS
@@ -89,4 +89,4 @@
 #error "CSP must use one of KISS or SDR"
 #endif /* !defined(CSP_USE_KISS) && !defined(CSP_USE_SDR) || defined(CSP_USE_KISS) && defined(CSP_USE_SDR) */
 
-#endif /* MAIN_CONFIG_H_ */
+#endif /* MAIN_CONFIG_FLATSAT_H_ */
--- main/config_yukonsat.h
+++ main/config_yukonsat.h
@@ -5,59 +5,59 @@
  *      Author: Robert
  */
 
-#ifndef CONFIG_H_
-#define CONFIG_H_
+#ifndef CONFIG_YUKONSAT_H_
+#define CONFIG_YUKONSAT_H_
 
 #define SYSTEM_APP_ID _OBC_APP_ID_
 
-#define IS_ATHENA 0
-#define IS_ATHENA_V2 0
+#define IS_ATHENA 1
+#define IS_ATHENA_V2 1
 
 #define IS_SATELLITE 1
 #if IS_SATELLITE == 1
 #define IS_FLATSAT 0
-#define IS_EXALTA2 1
+#define IS_EXALTA2 0
 #define IS_YUKONSAT 0
-#define IS_AURORASAT 0
+#define IS_AURORASAT 1
 #endif
 
-#define EXECUTE_LEOP 0
+#define EXECUTE_LEOP 1
 
-#define BOOTLOADER_PRESENT 0
-#define GOLDEN_IMAGE 1
-#define WORKING_IMAGE 0
+#define BOOTLOADER_PRESENT 1
+#define GOLDEN_IMAGE 0
+#define WORKING_IMAGE 1
 
 #define HAS_SD_CARD 1
 #if HAS_SD_CARD == 1
 #define SD_CARD_REFORMAT 0
 #endif
 
-#define ATHENA_IS_STUBBED 1
-#define UHF_IS_STUBBED 1
-#define ADCS_IS_STUBBED 1
-#define SBAND_IS_STUBBED 1
-#define EPS_IS_STUBBED 1
-#define HYPERION_IS_STUBBED 1
-#define CHARON_IS_STUBBED 1
-#define DFGM_IS_STUBBED 1
-#define WATCHDOG_IS_STUBBED 1
+#define ATHENA_IS_STUBBED 0
+#define UHF_IS_STUBBED 0
+#define ADCS_IS_STUBBED 0
+#define SBAND_IS_STUBBED 0
+#define EPS_IS_STUBBED 0
+#define HYPERION_IS_STUBBED 0
+#define CHARON_IS_STUBBED 0
+#define DFGM_IS_STUBBED 0
+#define WATCHDOG_IS_STUBBED 0
 #define PAYLOAD_IS_STUBBED 0
 
 #define HYPERION_PANEL_3U 0
-#define HYPERION_PANEL_2U 0
+#define HYPERION_PANEL_2U 1
 #define HYPERION_PANEL_2U_LIMITED 0
 
 #define IS_SN0072_EPS 0
 
 #define UHF_USE_I2C_CMDS 0
 
-#define SBAND_COMMERCIAL_FREQUENCY 1
+#define SBAND_COMMERCIAL_FREQUENCY 0
 
-#define ADCS_MAG_FLIGHT_CONFIG 0
+#define ADCS_MAG_FLIGHT_CONFIG 1
 
 #define CSP_FREERTOS 1
-#define CSP_USE_SDR 0
-#define CSP_USE_KISS 1
+#define CSP_USE_SDR 1
+#define CSP_USE_KISS 0
 
 /* Define SDR_NO_CSP==0 to use CSP for SDR */
 #define SDR_NO_CSP 0
@@ -89,4 +89,4 @@
 #error "CSP must use one of KISS or SDR"
 #endif /* !defined(CSP_USE_KISS) && !defined(CSP_USE_SDR) || defined(CSP_USE_KISS) && defined(CSP_USE_SDR) */
 
-#endif /* MAIN_CONFIG_H_ */
+#endif /* MAIN_CONFIG_YUKONSAT_H_ */
