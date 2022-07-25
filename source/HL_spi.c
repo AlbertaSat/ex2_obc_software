2ac5f533d9b7b0b6547b53be4d7deafe6bc0bcf6
Jenish Patel <jenish@ualberta.ca>
2022-07-25 10:33:16 -0600

    Add wdelay to iris-obc spi

--- source/HL_spi.c
+++ source/HL_spi.c
@@ -440,7 +440,7 @@ void spiInit(void)
                   | (uint32)((uint32)8U << 0U);  /* data word length */
 
     /** - Data Format 2 */
-    spiREG3->FMT2 = (uint32)((uint32)0U << 24U)  /* wdelay */
+    spiREG3->FMT2 = (uint32)((uint32)2U << 24U)  /* wdelay */
                   | (uint32)((uint32)0U << 23U)  /* parity Polarity */
                   | (uint32)((uint32)0U << 22U)  /* parity enable */
                   | (uint32)((uint32)0U << 21U)  /* wait on enable */
@@ -587,7 +587,7 @@ void spiInit(void)
                    | (uint32)((uint32)0U << 0U);  /* C2EDELAY */
 
     /** - Data Format 0 */
-    spiREG4->FMT0 = (uint32)((uint32)0U << 24U)  /* wdelay */
+    spiREG4->FMT0 = (uint32)((uint32)2U << 24U)  /* wdelay */
                   | (uint32)((uint32)0U << 23U)  /* parity Polarity */
                   | (uint32)((uint32)0U << 22U)  /* parity enable */
                   | (uint32)((uint32)0U << 21U)  /* wait on enable */
