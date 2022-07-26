cce0b5f7b7efc0e88b6d226bc3804658d9f3ac01
LiamDroog <droog@ualberta.ca>
2022-07-26 10:50:33 -0600

    Added defs, includes

--- ex2_hal/athena/equipment_handler/include/ina209.h
+++ ex2_hal/athena/equipment_handler/include/ina209.h
@@ -9,9 +9,13 @@
 #define EX2_HAL_ATHENA_EQUIPMENT_HANDLER_INCLUDE_INA209_H_
 
 #include <stdint.h>
+#include <FreeRTOS.h>
+#include <os_task.h>
 
 #define SOLAR_INA209_ADDR 0x40
 #define CORE_V_INA209_ADDR 0x45
+
+#define SOLAR_INA209_STACK_LEN 50 // arbitrary for now
 
 void ina209_get_configuration(uint8_t addr, uint16_t *retval);
 void ina209_set_configuration(uint8_t addr, uint16_t *val);
@@ -37,6 +41,5 @@ void ina209_get_calibration(uint8_t addr, uint16_t *retval);
 void ina209_set_calibration(uint8_t addr, uint16_t *val);
 void init_ina209(uint8_t addr);
 void reset_ina209(uint8_t addr);
-
 
 #endif /* EX2_HAL_ATHENA_EQUIPMENT_HANDLER_INCLUDE_INA209_H_ */
