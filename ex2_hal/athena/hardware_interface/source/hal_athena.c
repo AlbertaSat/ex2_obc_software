bf352bf84eee4a7e24bf00e1dfb523ff6921637d
LiamDroog <droog@ualberta.ca>
2022-07-26 10:53:34 -0600

    fix includes; add overcurrent checking function

--- ex2_hal/athena/hardware_interface/source/hal_athena.c
+++ ex2_hal/athena/hardware_interface/source/hal_athena.c
@@ -4,14 +4,32 @@
  *  Created on: Jul 18, 2022
  *      Author: joshd
  */
-#include "ina209.h"
+#include <hal_athena.h>
+#include <ina209.h>
+#include "HL_gio.h"
+#include "logger.h"
 
-uint16_t SOLAR_INA209_CONFIG  = 0xDA73;
+uint32_t is_oc;
 
-int initAthena(void){
+int initAthena(void) {
 #if IS_ATHENA_V2 == 1
+    // set pin high, ensuring FET is allowing current through
+    gioSetBit(SOLAR_CURRENTSENSE_SHDN_PORT, SOLAR_CURRENTSENSE_SHDN_PIN, 1);
     init_ina209(SOLAR_INA209_ADDR);
-    ina209_set_configuration((uint8_t)SOLAR_INA209_ADDR, &SOLAR_INA209_CONFIG);
     return 0;
 #endif
+}
+
+void is_SolarPanel_overcurrent(void *pvParameters) {
+    while (1) {
+        is_oc = gioGetBit(SOLAR_CURRENTSENSE_ALERT_PORT, SOLAR_CURRENTSENSE_ALERT_PIN);
+        if (is_oc != 1) {
+            // pull pin low to cut MOSFET
+            gioSetBit(SOLAR_CURRENTSENSE_SHDN_PORT, SOLAR_CURRENTSENSE_SHDN_PIN, 0);
+            sys_log(CRITICAL, "Solar panel overcurrent event occurred.");
+            vTaskDelete(0);
+        } else {
+            vTaskDelay(300);
+        }
+    }
 }
