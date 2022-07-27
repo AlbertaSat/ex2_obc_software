c46691592f27c426e0c92c6126e8ad9351c66d0f
LiamDroog <droog@ualberta.ca>
2022-07-27 14:29:34 -0600

    add edge case check

--- ex2_hal/athena/hardware_interface/source/hal_athena.c
+++ ex2_hal/athena/hardware_interface/source/hal_athena.c
@@ -2,7 +2,7 @@
  * hal_athena.c
  *
  *  Created on: Jul 18, 2022
- *      Author: joshd
+ *      Author: Liam Droog
  */
 #include <hal_athena.h>
 #include <ina209.h>
@@ -14,29 +14,30 @@ uint32_t is_oc;
 
 int initAthena(void) {
 #if IS_ATHENA_V2 == 1
-
-    // check eeprom flag for is_sensors_fucked
-
     init_ina209(SOLAR_INA209_ADDR);
-
     // set pin high, ensuring FET is allowing current through
     gioSetBit(SOLAR_CURRENTSENSE_SHDN_PORT, SOLAR_CURRENTSENSE_SHDN_PIN, 1);
 
-    return 0;
+    // check pin to ensure there isn't an overcurrent event
+    is_oc = gioGetBit(SOLAR_CURRENTSENSE_ALERT_PORT, SOLAR_CURRENTSENSE_ALERT_PIN);
+    if (is_oc != 1) {
+        // pull pin low to cut MOSFET
+        gioSetBit(SOLAR_CURRENTSENSE_SHDN_PORT, SOLAR_CURRENTSENSE_SHDN_PIN, 0);
+        sys_log(CRITICAL, "Solar panel overcurrent event occurred.");
 #endif
-}
+        return 0;
+    }
 
-void is_SolarPanel_overcurrent(void *pvParameters) {
-    while (1) {
-        is_oc = gioGetBit(SOLAR_CURRENTSENSE_ALERT_PORT, SOLAR_CURRENTSENSE_ALERT_PIN);
-        if (is_oc != 1) {
-            // pull pin low to cut MOSFET
-            gioSetBit(SOLAR_CURRENTSENSE_SHDN_PORT, SOLAR_CURRENTSENSE_SHDN_PIN, 0);
-            sys_log(CRITICAL, "Solar panel overcurrent event occurred.");
-            // add eeprom flag saying panels are effed//
-            vTaskDelete(0);
-        } else {
-            vTaskDelay(300);
+    void is_SolarPanel_overcurrent(void *pvParameters) {
+        while (1) {
+            is_oc = gioGetBit(SOLAR_CURRENTSENSE_ALERT_PORT, SOLAR_CURRENTSENSE_ALERT_PIN);
+            if (is_oc != 1) {
+                // pull pin low to cut MOSFET
+                gioSetBit(SOLAR_CURRENTSENSE_SHDN_PORT, SOLAR_CURRENTSENSE_SHDN_PIN, 0);
+                sys_log(CRITICAL, "Solar panel overcurrent event occurred.");
+                vTaskDelete(0);
+            } else {
+                vTaskDelay(300);
+            }
         }
     }
-}
