460f1b2292b4ce40d8e59c36ced75621fa987d53
LiamDroog <droog@ualberta.ca>
2022-07-27 14:10:58 -0600

    Tweak init order

--- ex2_hal/athena/hardware_interface/source/hal_athena.c
+++ ex2_hal/athena/hardware_interface/source/hal_athena.c
@@ -8,14 +8,20 @@
 #include <ina209.h>
 #include "HL_gio.h"
 #include "logger.h"
+#include "ina209.h"
 
 uint32_t is_oc;
 
 int initAthena(void) {
 #if IS_ATHENA_V2 == 1
+
+    // check eeprom flag for is_sensors_fucked
+
+    init_ina209(SOLAR_INA209_ADDR);
+
     // set pin high, ensuring FET is allowing current through
     gioSetBit(SOLAR_CURRENTSENSE_SHDN_PORT, SOLAR_CURRENTSENSE_SHDN_PIN, 1);
-    init_ina209(SOLAR_INA209_ADDR);
+
     return 0;
 #endif
 }
@@ -27,6 +33,7 @@ void is_SolarPanel_overcurrent(void *pvParameters) {
             // pull pin low to cut MOSFET
             gioSetBit(SOLAR_CURRENTSENSE_SHDN_PORT, SOLAR_CURRENTSENSE_SHDN_PIN, 0);
             sys_log(CRITICAL, "Solar panel overcurrent event occurred.");
+            // add eeprom flag saying panels are effed//
             vTaskDelete(0);
         } else {
             vTaskDelay(300);
