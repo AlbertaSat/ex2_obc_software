2613fbd8097de764da27bcdfb9522b39c9d69a57
LiamDroog <droog@ualberta.ca>
2022-07-27 14:57:12 -0600

    found rogue semicolon

--- ex2_hal/athena/hardware_interface/source/hal_athena.c
+++ ex2_hal/athena/hardware_interface/source/hal_athena.c
@@ -27,17 +27,18 @@ int initAthena(void) {
 #endif
         return 0;
     }
+}
 
-    void is_SolarPanel_overcurrent(void *pvParameters) {
-        while (1) {
-            is_oc = gioGetBit(SOLAR_CURRENTSENSE_ALERT_PORT, SOLAR_CURRENTSENSE_ALERT_PIN);
-            if (is_oc != 1) {
-                // pull pin low to cut MOSFET
-                gioSetBit(SOLAR_CURRENTSENSE_SHDN_PORT, SOLAR_CURRENTSENSE_SHDN_PIN, 0);
-                sys_log(CRITICAL, "Solar panel overcurrent event occurred.");
-                vTaskDelete(0);
-            } else {
-                vTaskDelay(300);
-            }
+void is_SolarPanel_overcurrent(void *pvParameters) {
+    while (1) {
+        is_oc = gioGetBit(SOLAR_CURRENTSENSE_ALERT_PORT, SOLAR_CURRENTSENSE_ALERT_PIN);
+        if (is_oc != 1) {
+            // pull pin low to cut MOSFET
+            gioSetBit(SOLAR_CURRENTSENSE_SHDN_PORT, SOLAR_CURRENTSENSE_SHDN_PIN, 0);
+            sys_log(CRITICAL, "Solar panel overcurrent event occurred.");
+            vTaskDelete(0);
+        } else {
+            vTaskDelay(300);
         }
     }
+}
