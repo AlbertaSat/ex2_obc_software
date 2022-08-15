b869765177e04e340b4c4e59a15a4f81159b9c6b
Robert Taylor <taylorr@ualberta.ca>
2022-08-15 15:31:52 -0600

    Use explicit versioning

---
+++ main/version.h
@@ -0,0 +1,11 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#include <stdint.h>
+
+const char *version = "0.9.0";
+const uint16_t version_major = 0;
+const uint16_t version_minor = 9;
+const uint16_t version_patch = 0;
+
+#endif // VERSION_H
