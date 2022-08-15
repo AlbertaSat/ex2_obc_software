510b163503d2e478aaca6a92613be490ce284ca2
Robert Taylor <taylorr@ualberta.ca>
2022-08-15 16:09:43 -0600

    Use defines instead

--- main/version.h
+++ main/version.h
@@ -3,9 +3,9 @@
 
 #include <stdint.h>
 
-const char *version = "0.9.0";
-const uint16_t version_major = 0;
-const uint16_t version_minor = 9;
-const uint16_t version_patch = 0;
+#define VERSION "0.9.0"
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 9
+#define VERSION_PATCH 0
 
 #endif // VERSION_H
