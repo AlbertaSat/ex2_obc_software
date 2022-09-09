415174d4b3e9e290120b7d979dbb0ad463d09c69
Robert Taylor <taylorr@ualberta.ca>
2022-09-09 14:59:37 -0600

    Use correct ifdef

--- source/HL_notification.c
+++ source/HL_notification.c
@@ -223,7 +223,7 @@ void sciNotification(sciBASE_t *sci, uint32 flags)
 /* USER CODE BEGIN (32) */
     uint32_t int_reg = (uint32_t)sci;
     switch(int_reg) {
-#if IS_EXALTA2 == 1
+#if NS_IS_STUBBED == 1
     case (uint32_t)GPS_SCI: gps_sciNotification(sci, flags); break;
 #else
     case (uint32_t)PAYLOAD_SCI: ns_sciNotification(sci, flags); break;
