eace76572d4a457dd346ebf187fb4c6a41671e74
Robert Taylor <taylorr@ualberta.ca>
2022-09-15 12:06:05 -0600

    Use a different base64 with static decoding table

--- source/base_64.c
+++ source/base_64.c
@@ -2,26 +2,41 @@
  * b64_encode_decode.c
  *
  *  Created on: Nov. 29, 2021
- *      Author: Grace Yi
+ *      Author: Grace Yi, https://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c
  */
 #include "base_64.h"
+#include "FreeRTOS.h"
 
-static const char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                                'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+static const char encoding_table[] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+    'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+    's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
 
-static char *decoding_table = NULL;
-static int mod_table[] = {0, 2, 1};
-
-static void build_decoding_table();
-static void base64_cleanup();
+static const unsigned char decoding_table[256] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3e, 0x00, 0x00, 0x00, 0x3f, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
+    0x3a, 0x3b, 0x3c, 0x3d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+    0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
+    0x19, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24,
+    0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00};
 
 char *base64_encode(const unsigned char *data, size_t input_length, size_t *output_length) {
 
-    *output_length = (input_length + 2) * 8 / 6; /* 3-byte blocks to 4-byte */
+    const int mod_table[] = {0, 2, 1};
 
-    char *encoded_data = (char *)pvPortMalloc(*output_length);
+    *output_length = 4 * ((input_length + 2) / 3);
+
+    char *encoded_data = (char *)pvPortMalloc(output_length);
+
     if (encoded_data == NULL)
         return NULL;
 
@@ -43,30 +58,24 @@ char *base64_encode(const unsigned char *data, size_t input_length, size_t *outp
         encoded_data[*output_length - 1 - i] = '=';
 
     return encoded_data;
-}
+};
 
 unsigned char *base64_decode(const char *data, size_t input_length, size_t *output_length) {
 
-    if (decoding_table == NULL) {
-        build_decoding_table();
-    }
-
-    if (input_length % 4 != 0) {
+    if (input_length % 4 != 0)
         return NULL;
-    }
 
     *output_length = input_length / 4 * 3;
-    if (data[input_length - 1] == '=') {
-        (*output_length)--;
-    }
-    if (data[input_length - 2] == '=') {
-        (*output_length)--;
-    }
 
-    unsigned char *decoded_data = (unsigned char *)pvPortMalloc(*output_length);
-    if (decoded_data == NULL) {
+    if (data[input_length - 1] == '=')
+        (output_length)--;
+    if (data[input_length - 2] == '=')
+        (output_length)--;
+
+    unsigned char *decoded_data = (unsigned char *)pvPortMalloc(output_length);
+
+    if (decoded_data == NULL)
         return NULL;
-    }
 
     for (int i = 0, j = 0; i < input_length;) {
 
@@ -77,27 +86,13 @@ unsigned char *base64_decode(const char *data, size_t input_length, size_t *outp
 
         uint32_t triple = (sextet_a << 3 * 6) + (sextet_b << 2 * 6) + (sextet_c << 1 * 6) + (sextet_d << 0 * 6);
 
-        if (j < *output_length) {
+        if (j < (int)output_length)
             decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
-        }
-        if (j < *output_length) {
+        if (j < (int)output_length)
             decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
-        }
-        if (j < *output_length) {
+        if (j < (int)output_length)
             decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
-        }
     }
 
     return decoded_data;
-}
-
-static void build_decoding_table() {
-
-    decoding_table = pvPortMalloc(256);
-
-    for (int i = 0; i < 64; i++) {
-        decoding_table[(unsigned char)encoding_table[i]] = i;
-    }
-}
-
-static void base64_cleanup() { vPortFree(decoding_table); }
+};
