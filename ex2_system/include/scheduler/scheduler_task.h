3a3a8a6d767d0d381c7274ea190e317b8d54a7e5
Ron Unrau <rcunrau@ualberta.ca>
2023-02-09 09:19:31 -0700

    scheduler_task.[ch]: execute the schedule from the schedule file

--- ex2_system/include/scheduler/scheduler_task.h
+++ ex2_system/include/scheduler/scheduler_task.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020  University of Alberta
+ * Copyright (C) 2022-2023  University of Alberta
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -19,22 +19,14 @@
 #ifndef SCHEDULER_TASK_H_
 #define SCHEDULER_TASK_H_
 
-// TODO: determine the required stack size
-#define SCHEDULER_STACK_SIZE 12000
-#define MAX_DATA_LEN 12 // TODO: determine if this is the best max length
-#define SCHEDULE_HISTORY_SIZE                                                                                     \
-    100 // number of most recent executed commands that will be stored in the history.
-        // once this # is exceeded, commands in the beginning of the file will be overwritten.
-        // eg. the 102th command will overwrite the 2nd command.
-#include "system.h"
-#include <os_queue.h>
 #include <FreeRTOS.h>
-#include <os_semphr.h>
-#include "scheduler/scheduler.h"
-#include <redposix.h> //include for file system
-#include "csp_types.h"
+#include "os_queue.h"
+#include "system.h"
 
-extern char *fileName2;
-extern char *fileName3;
+extern QueueHandle_t SchedulerNotificationQueue;
+
+#define SCHEDULER_STACK_SIZE 12000
+
+SAT_returnState start_scheduler_task(void);
 
 #endif /* SCHEDULER_TASK_H_ */
